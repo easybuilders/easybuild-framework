f090386b5c4a383fa55cd17caccb9de03356f3a6
Jens Timmerman <jens.timmerman@ugent.be>
2012-10-19 16:29:49 +0200

    fixed namespace for framework

--- easybuild/__init__.py
+++ easybuild/__init__.py
@@ -29,7 +29,12 @@ appended by the git commit id in VERBOSE_VERSION.
 """
 from distutils.version import LooseVersion
 import os
+from pkgutil import extend_path
 import sys
+
+
+# we're not the only ones in this namespace
+__path__ = extend_path(__path__, __name__)
 
 VERSION = LooseVersion("0.9")
 UNKNOWN = "UNKNOWN"
