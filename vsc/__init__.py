8b27069fa2d14411a3301df14bdf860b96e64ca2
Jens Timmerman <jens.timmerman@ugent.be>
2012-10-23 16:58:39 +0200

    added github.py, this contains a flexible and easy to use github api

--- vsc/__init__.py
+++ vsc/__init__.py
@@ -22,10 +22,3 @@
 # You should have received a copy of the GNU General Public License
 # along with VSC-tools. If not, see <http://www.gnu.org/licenses/>.
 ##
-"""
-Initialize vsc package.
-the vsc namespace is used in different folders allong the system
-so explicitly declare this is also the vsc namespace
-"""
-import pkg_resources
-pkg_resources.declare_namespace(__name__)
