b43f3ea633d70af7c17b997d55f2fbfd9018c203
Kenneth Hoste <kenneth.hoste@ugent.be>
2012-11-05 16:37:44 +0100

    check types with isinstance

--- easybuild/test/toolchain.py
+++ easybuild/test/toolchain.py
@@ -79,9 +79,9 @@ class ToolchainTest(TestCase):
         tc.prepare()
 
         ldflags = tc.get_variable('LDFLAGS', typ=list)
-        self.assertTrue(type(ldflags) == list)
+        self.assertTrue(isinstance(ldflags, list))
         if len(ldflags) > 0:
-            self.assertTrue(type(ldflags[0]) == basestring)
+            self.assertTrue(isinstance(ldflags[0], basestring))
 
     def tearDown(self):
         """Cleanup."""
