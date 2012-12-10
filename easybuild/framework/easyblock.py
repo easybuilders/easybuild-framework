39478262b48c9f4567fcf2c551bfcc1729884b0e
Kenneth Hoste <kenneth.hoste@ugent.be>
2012-12-10 14:34:46 +0100

    fix typo in passing of arguments

--- easybuild/framework/easyblock.py
+++ easybuild/framework/easyblock.py
@@ -707,7 +707,7 @@ class EasyBlock(object):
         # set environment variable that specifies list of extensions
         if self.exts:
             exts_list = ','.join(['%s-%s' % (ext['name'], ext['version']) for ext in self.exts])
-            txt += self.moduleGenerator.set_environment('EBEXTSLIST%s', (exts_list, self.name.upper()))
+            txt += self.moduleGenerator.set_environment('EBEXTSLIST%s' % self.name.upper(), exts_list)
 
         return txt
 
