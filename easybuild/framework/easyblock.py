54ab6011946a0f55c51bec6fa3dc31b1c11f3987
stdweird <stijn.deweirdt@ugent.be>
2013-02-19 10:25:20 +0100

    restore license header

--- easybuild/framework/easyblock.py
+++ easybuild/framework/easyblock.py
@@ -25,6 +25,7 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
+# You should have received a copy of the GNU General Public License
 # along with EasyBuild.  If not, see <http://www.gnu.org/licenses/>.
 ##
 """
