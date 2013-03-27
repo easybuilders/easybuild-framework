bb554a4a86205c7c44605e3c88584c0457f65525
stdweird <stijn.deweirdt@ugent.be>
2013-03-27 15:24:01 +0100

    fix indent

--- easybuild/tools/toolchain/constants.py
+++ easybuild/tools/toolchain/constants.py
@@ -35,33 +35,33 @@ from easybuild.tools.toolchain.variables import FlagList, LibraryList
 
 
 COMPILER_VARIABLES = [
-                      ('CC', 'C compiler'),
-                      ('CXX', 'C++ compiler'),
-                      ('F77', 'Fortran 77 compiler'),
-                      ('F90', 'Fortran 90 compiler'),
-                      ]
+    ('CC', 'C compiler'),
+    ('CXX', 'C++ compiler'),
+    ('F77', 'Fortran 77 compiler'),
+    ('F90', 'Fortran 90 compiler'),
+]
 
 COMPILER_MAP_CLASS = {
-                      FlagList: [
-                                 ('OPTFLAGS', 'Optimization flags'),
-                                 ('PRECFLAGS', 'FP precision flags'),
-                                 ('CFLAGS', 'C compiler flags'),
-                                 ('CXXFLAGS', 'C++ compiler flags'),
-                                 ('FFLAGS', 'Fortran compiler flags'),
-                                 ('F90FLAGS', 'Fortran 90 compiler flags'),
-                                ],
-                      LibraryList: [
-                                    ('LIBS', 'Libraries'),  # TODO: where are these used? ld?
-                                    ('FLIBS', 'Fortran libraries'),  # TODO: where are these used? gfortran only?
-                                   ],
-                      LinkLibraryPaths: [
-                                         ('LDFLAGS', 'Flags passed to linker'),  # TODO: overridden by command line?
-                                        ],
-                      IncludePaths: [
-                                     ('CPPFLAGS', 'Precompiler flags'),
-                                    ],
-                      CommandFlagList: COMPILER_VARIABLES,
-                      }
+    FlagList: [
+        ('OPTFLAGS', 'Optimization flags'),
+        ('PRECFLAGS', 'FP precision flags'),
+        ('CFLAGS', 'C compiler flags'),
+        ('CXXFLAGS', 'C++ compiler flags'),
+        ('FFLAGS', 'Fortran compiler flags'),
+        ('F90FLAGS', 'Fortran 90 compiler flags'),
+    ],
+    LibraryList: [
+        ('LIBS', 'Libraries'),  # TODO: where are these used? ld?
+        ('FLIBS', 'Fortran libraries'),  # TODO: where are these used? gfortran only?
+    ],
+    LinkLibraryPaths: [
+        ('LDFLAGS', 'Flags passed to linker'),  # TODO: overridden by command line?
+    ],
+    IncludePaths: [
+        ('CPPFLAGS', 'Precompiler flags'),
+    ],
+    CommandFlagList: COMPILER_VARIABLES,
+}
 
 MPI_COMPILER_TEMPLATE = "MPI%(c_var)s"
 MPI_COMPILER_VARIABLES = [(MPI_COMPILER_TEMPLATE % {'c_var': v}, "MPI %s wrapper" % d)
@@ -72,111 +72,111 @@ SEQ_COMPILER_VARIABLES = [(SEQ_COMPILER_TEMPLATE % {'c_var': v}, "sequential %s"
                           for (v, d) in COMPILER_VARIABLES]
 
 MPI_MAP_CLASS = {
-                 AbsPathList: [
-                               # TODO: useful at all? shouldn't these be obtained from mpiXX --show
-                               ('MPI_LIB_STATIC', 'MPI libraries (static)'),
-                               ('MPI_LIB_SHARED', 'MPI libraries (shared)'),
-                               ('MPI_LIB_DIR', 'MPI library directory'),
-                               ('MPI_INC_DIR', 'MPI include directory'),
-                               ],
-                 CommandFlagList: MPI_COMPILER_VARIABLES + SEQ_COMPILER_VARIABLES,
-                 }
+    AbsPathList: [
+        # TODO: useful at all? shouldn't these be obtained from mpiXX --show
+        ('MPI_LIB_STATIC', 'MPI libraries (static)'),
+        ('MPI_LIB_SHARED', 'MPI libraries (shared)'),
+        ('MPI_LIB_DIR', 'MPI library directory'),
+        ('MPI_INC_DIR', 'MPI include directory'),
+    ],
+    CommandFlagList: MPI_COMPILER_VARIABLES + SEQ_COMPILER_VARIABLES,
+}
 
 BLAS_MAP_CLASS = {
-                  AbsPathList:[
-                               ('BLAS_LIB_DIR', 'BLAS library directory'),
-                               ('BLAS_INC_DIR', 'BLAS include directory'),
+    AbsPathList: [
+        ('BLAS_LIB_DIR', 'BLAS library directory'),
+        ('BLAS_INC_DIR', 'BLAS include directory'),
 
-                               ],
-                  LibraryList:[
-                               ('LIBBLAS', 'BLAS libraries'),
-                               ('LIBBLAS_MT', 'multithreaded BLAS libraries'),
-                               ],
-                  CommaStaticLibs:[
-                                   ('BLAS_STATIC_LIBS', 'Comma-separated list of static BLAS libraries'),
-                                   ('BLAS_MT_STATIC_LIBS', 'Comma-separated list of static multithreaded BLAS libraries'),
-                                   ],
-                  }
+    ],
+    LibraryList: [
+        ('LIBBLAS', 'BLAS libraries'),
+        ('LIBBLAS_MT', 'multithreaded BLAS libraries'),
+    ],
+    CommaStaticLibs: [
+        ('BLAS_STATIC_LIBS', 'Comma-separated list of static BLAS libraries'),
+        ('BLAS_MT_STATIC_LIBS', 'Comma-separated list of static multithreaded BLAS libraries'),
+    ],
+}
 LAPACK_MAP_CLASS = {
-                    AbsPathList:[
-                                 ('LAPACK_LIB_DIR', 'LAPACK library directory'),
-                                 ('LAPACK_INC_DIR', 'LAPACK include directory'),
-                                 ('BLAS_LAPACK_LIB_DIR', 'BLAS and LAPACK library directory'),
-                                 ('BLAS_LAPACK_INC_DIR', 'BLAS and LAPACK include directory'),
-                                 ],
-                    LibraryList:[
-                                 ('LIBLAPACK_ONLY', 'LAPACK libraries (LAPACK only)'),
-                                 ('LIBLAPACK_MT_ONLY', 'multithreaded LAPACK libraries (LAPACK only)'),
-                                 ('LIBLAPACK', 'LAPACK libraries'),
-                                 ('LIBLAPACK_MT', 'multithreaded LAPACK libraries'),
-                                 ],
-                    CommaStaticLibs:[
-                                     ('LAPACK_STATIC_LIBS', 'Comma-separated list of static LAPACK libraries'),
-                                     ('LAPACK_MT_STATIC_LIBS', 'Comma-separated list of static LAPACK libraries'),
-                                     ('BLAS_LAPACK_STATIC_LIBS', 'Comma-separated list of static BLAS and LAPACK libraries'),
-                                     ('BLAS_LAPACK_MT_STATIC_LIBS', 'Comma-separated list of static BLAS and LAPACK libraries'),
-                                     ],
-                    }
+    AbsPathList: [
+        ('LAPACK_LIB_DIR', 'LAPACK library directory'),
+        ('LAPACK_INC_DIR', 'LAPACK include directory'),
+        ('BLAS_LAPACK_LIB_DIR', 'BLAS and LAPACK library directory'),
+        ('BLAS_LAPACK_INC_DIR', 'BLAS and LAPACK include directory'),
+    ],
+    LibraryList: [
+        ('LIBLAPACK_ONLY', 'LAPACK libraries (LAPACK only)'),
+        ('LIBLAPACK_MT_ONLY', 'multithreaded LAPACK libraries (LAPACK only)'),
+        ('LIBLAPACK', 'LAPACK libraries'),
+        ('LIBLAPACK_MT', 'multithreaded LAPACK libraries'),
+    ],
+    CommaStaticLibs: [
+        ('LAPACK_STATIC_LIBS', 'Comma-separated list of static LAPACK libraries'),
+        ('LAPACK_MT_STATIC_LIBS', 'Comma-separated list of static LAPACK libraries'),
+        ('BLAS_LAPACK_STATIC_LIBS', 'Comma-separated list of static BLAS and LAPACK libraries'),
+        ('BLAS_LAPACK_MT_STATIC_LIBS', 'Comma-separated list of static BLAS and LAPACK libraries'),
+    ],
+}
 BLACS_MAP_CLASS = {
-                  AbsPathList:[
-                               ('BLACS_LIB_DIR', 'BLACS library directory'),
-                               ('BLACS_INC_DIR', 'BLACS include directory'),
+    AbsPathList: [
+        ('BLACS_LIB_DIR', 'BLACS library directory'),
+        ('BLACS_INC_DIR', 'BLACS include directory'),
 
-                               ],
-                  LibraryList:[
-                               ('LIBBLACS', 'BLACS libraries'),
-                               ('LIBBLACS_MT', 'multithreaded BLACS libraries'),
-                               ],
-                  CommaStaticLibs:[
-                                   ('BLACS_STATIC_LIBS', 'Comma-separated list of static BLACS libraries'),
-                                   ('BLACS_MT_STATIC_LIBS', 'Comma-separated list of static multithreaded BLACS libraries'),
-                                   ],
-                  }
+    ],
+    LibraryList: [
+        ('LIBBLACS', 'BLACS libraries'),
+        ('LIBBLACS_MT', 'multithreaded BLACS libraries'),
+    ],
+    CommaStaticLibs: [
+        ('BLACS_STATIC_LIBS', 'Comma-separated list of static BLACS libraries'),
+        ('BLACS_MT_STATIC_LIBS', 'Comma-separated list of static multithreaded BLACS libraries'),
+    ],
+}
 
 SCALAPACK_MAP_CLASS = {
-                    AbsPathList:[
-                                 ('SCALAPACK_LIB_DIR', 'SCALAPACK library directory'),
-                                 ('SCALAPACK_INC_DIR', 'SCALAPACK include directory'),
-                                 ],
-                    LibraryList:[
-                                 ('LIBSCALAPACK_ONLY', 'SCALAPACK libraries (SCALAPACK only)'),
-                                 ('LIBSCALAPACK_MT_ONLY', 'multithreaded SCALAPACK libraries (SCALAPACK only)'),
-                                 ('LIBSCALAPACK', 'SCALAPACK libraries'),
-                                 ('LIBSCALAPACK_MT', 'multithreaded SCALAPACK libraries'),
-                                 ],
-                       CommaStaticLibs:[
-                                        ('SCALAPACK_STATIC_LIBS', 'Comma-separated list of static SCALAPACK libraries'),
-                                        ('SCALAPACK_MT_STATIC_LIBS', 'Comma-separated list of static SCALAPACK libraries'),
-                                        ],
-                       }
+    AbsPathList: [
+        ('SCALAPACK_LIB_DIR', 'SCALAPACK library directory'),
+        ('SCALAPACK_INC_DIR', 'SCALAPACK include directory'),
+    ],
+    LibraryList: [
+        ('LIBSCALAPACK_ONLY', 'SCALAPACK libraries (SCALAPACK only)'),
+        ('LIBSCALAPACK_MT_ONLY', 'multithreaded SCALAPACK libraries (SCALAPACK only)'),
+        ('LIBSCALAPACK', 'SCALAPACK libraries'),
+        ('LIBSCALAPACK_MT', 'multithreaded SCALAPACK libraries'),
+    ],
+    CommaStaticLibs: [
+        ('SCALAPACK_STATIC_LIBS', 'Comma-separated list of static SCALAPACK libraries'),
+        ('SCALAPACK_MT_STATIC_LIBS', 'Comma-separated list of static SCALAPACK libraries'),
+    ],
+}
 
 FFT_MAP_CLASS = {
-                  AbsPathList:[
-                               ('FFT_LIB_DIR', 'FFT library directory'),
-                               ('FFT_INC_DIR', 'FFT include directory'),
-                               ],
-                  LibraryList:[
-                               ('LIBFFT', 'FFT libraries'),
-                               ],
-                  CommaStaticLibs:[
-                                   ('FFT_STATIC_LIBS', 'Comma-separated list of static FFT libraries'),
-                                   ],
-                 }
+    AbsPathList: [
+        ('FFT_LIB_DIR', 'FFT library directory'),
+        ('FFT_INC_DIR', 'FFT include directory'),
+    ],
+    LibraryList: [
+        ('LIBFFT', 'FFT libraries'),
+    ],
+    CommaStaticLibs: [
+        ('FFT_STATIC_LIBS', 'Comma-separated list of static FFT libraries'),
+    ],
+}
 
 FFTW_MAP_CLASS = {
-                  AbsPathList:[
-                               ('FFTW_LIB_DIR', 'FFTW library directory'),
-                               ('FFTW_INC_DIR', 'FFTW include directory'),
-                               ],
-                  CommaStaticLibs:[
-                                   ('FFTW_STATIC_LIBS', 'Comma-separated list of static FFTW libraries'),
-                                   ],
-                  }
+    AbsPathList: [
+        ('FFTW_LIB_DIR', 'FFTW library directory'),
+        ('FFTW_INC_DIR', 'FFTW include directory'),
+    ],
+    CommaStaticLibs: [
+        ('FFTW_STATIC_LIBS', 'Comma-separated list of static FFTW libraries'),
+    ],
+}
 
 ALL_MAP_CLASSES = [
-                   COMPILER_MAP_CLASS, MPI_MAP_CLASS,
-                   BLAS_MAP_CLASS, LAPACK_MAP_CLASS, BLACS_MAP_CLASS, SCALAPACK_MAP_CLASS,
-                   FFT_MAP_CLASS, FFTW_MAP_CLASS,
-                   ]
+    COMPILER_MAP_CLASS, MPI_MAP_CLASS,
+    BLAS_MAP_CLASS, LAPACK_MAP_CLASS, BLACS_MAP_CLASS, SCALAPACK_MAP_CLASS,
+    FFT_MAP_CLASS, FFTW_MAP_CLASS,
+]
 
 
