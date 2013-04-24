bf89e2fedb17e0deb2da6c5626aee7c7fd6fff0b
Kenneth Hoste <kenneth.hoste@ugent.be>
2013-04-24 19:03:17 +0200

    split up filetools unit test suite into individual tests, add test to check run_cmd functionality when PROFILEREAD is set

--- test/framework/filetools.py
+++ test/framework/filetools.py
@@ -30,7 +30,7 @@ Unit tests for filetools.py
 @author: Stijn De Weirdt (Ghent University)
 """
 import os
-from unittest import TestCase, TestSuite, main
+from unittest import TestCase, TestLoader, main
 from vsc import fancylogger
 
 import easybuild.tools.config as config
@@ -58,11 +58,8 @@ class FileToolsTest(TestCase):
         """cleanup"""
         os.chdir(self.cwd)
 
-    def runTest(self):
-        """
-        verify all the possible extract commands
-        also run_cmd should work with some basic echo/exit combos
-        """
+    def test_extract_cmd(self):
+        """Test various extract commands."""
         cmd = ft.extract_cmd("test.zip")
         self.assertEqual("unzip -qq test.zip", cmd)
 
@@ -84,40 +81,70 @@ class FileToolsTest(TestCase):
         cmd = ft.extract_cmd("test.tar.bz2")
         self.assertEqual("tar xjf test.tar.bz2", cmd)
 
+    def test_run_cmd(self):
+        """Basic test for run_cmd function."""
         (out, ec) = ft.run_cmd("echo hello")
         self.assertEqual(out, "hello\n")
         # no reason echo hello could fail
         self.assertEqual(ec, 0)
 
+    def test_run_cmd_bis(self):
+        """More 'complex' test for run_cmd function."""
         # a more 'complex' command to run, make sure all required output is there
         (out, ec) = ft.run_cmd("for j in `seq 1 3`; do for i in `seq 1 100`; do echo hello; done; sleep 1.4; done")
         self.assertTrue(out.startswith('hello\nhello\n'))
         self.assertEqual(len(out), len("hello\n"*300))
         self.assertEqual(ec, 0)
 
+    def test_run_cmd_qa(self):
+        """Basic test for run_cmd_qa function."""
         (out, ec) = ft.run_cmd_qa("echo question; read x; echo $x", {"question": "answer"})
         self.assertEqual(out, "question\nanswer\n")
         # no reason echo hello could fail
         self.assertEqual(ec, 0)
 
+    def test_run_cmd_simple(self):
+        """Test return value for run_cmd in 'simple' mode."""
         self.assertEqual(True, ft.run_cmd("echo hello", simple=True))
         self.assertEqual(False, ft.run_cmd("exit 1", simple=True, log_all=False, log_ok=False))
 
+    def test_convert_name(self):
+        """Test convert_name function."""
         name = ft.convert_name("test+test-test")
         self.assertEqual(name, "testplustestmintest")
         name = ft.convert_name("test+test-test", True)
         self.assertEqual(name, "TESTPLUSTESTMINTEST")
 
-
+    def test_parse_log_error(self):
+        """Test basic parse_log_for_error functionality."""
         errors = ft.parse_log_for_error("error failed", True)
         self.assertEqual(len(errors), 1)
 
         # I expect tests to be run from the base easybuild directory
         self.assertEqual(os.getcwd(), ft.find_base_dir())
 
+    def test_run_cmd_suse(self):
+        """Test run_cmd on SuSE systems, which have $PROFILEREAD set."""
+        # run_cmd should also work if $PROFILEREAD is set (very relevant for SuSE systems)
+        profileread = os.environ.get('PROFILEREAD', None)
+        os.environ['PROFILEREAD'] = 'profilereadxxx'
+        try:
+            (out, ec) = ft.run_cmd("echo hello")
+        except Exception, err:
+            out, ec = "ERROR: %s" % err, 1
+
+        # make sure it's restored again before we can fail the test
+        if profileread is not None:
+            os.environ['PROFILEREAD'] = profileread
+        else:
+            del os.environ['PROFILEREAD']
+
+        self.assertEqual(out, "hello\n")
+        self.assertEqual(ec, 0)
+
 def suite():
     """ returns all the testcases in this module """
-    return TestSuite([FileToolsTest()])
+    return TestLoader().loadTestsFromTestCase(FileToolsTest)
 
 if __name__ == '__main__':
     main()
