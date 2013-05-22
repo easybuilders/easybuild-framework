c00d756e9d33f2ad6ef328c458f26e60b60b53aa
Jens Timmerman <jens.timmerman@ugent.be>
2013-05-22 16:32:18 +0200

    fixed testcases

--- test/framework/filetools.py
+++ test/framework/filetools.py
@@ -30,6 +30,7 @@ Unit tests for filetools.py
 @author: Stijn De Weirdt (Ghent University)
 """
 import os
+import tempfile
 from unittest import TestCase, TestLoader, main
 from vsc import fancylogger
 
@@ -40,6 +41,14 @@ from test.framework.utilities import find_full_path
 
 class FileToolsTest(TestCase):
     """ Testcase for filetools module """
+
+    CLASS_NAMES = [
+        ('GCC', 'EB_GCC'),
+        ('7zip', 'EB_7zip'),
+        ('Charm++', 'EB_Charm_plus__plus_'),
+        ('DL_POLY_Classic', 'EB_DL_underscore_POLY_underscore_Classic'),
+        ('0_foo+0x0x#-$__', 'EB_0_underscore_foo_plus_0x0x_hash__minus__dollar__underscore__underscore_'),
+    ]
 
     def setUp(self):
         self.log = fancylogger.getLogger(self.__class__.__name__)
@@ -64,35 +73,127 @@ class FileToolsTest(TestCase):
         """cleanup"""
         os.chdir(self.cwd)
 
-    def test_runcmd(self):
-        """
-        verify all the possible extract commands
-        also run_cmd should work with some basic echo/exit combos
-        """
+    def test_extract_cmd(self):
+        """Test various extract commands."""
+        cmd = ft.extract_cmd("test.zip")
+        self.assertEqual("unzip -qq test.zip", cmd)
 
+        cmd = ft.extract_cmd("/some/path/test.tar")
+        self.assertEqual("tar xf /some/path/test.tar", cmd)
+
+        cmd = ft.extract_cmd("test.tar.gz")
+        self.assertEqual("tar xzf test.tar.gz", cmd)
+
+        cmd = ft.extract_cmd("test.tgz")
+        self.assertEqual("tar xzf test.tgz", cmd)
+
+        cmd = ft.extract_cmd("test.bz2")
+        self.assertEqual("bunzip2 test.bz2", cmd)
+
+        cmd = ft.extract_cmd("test.tbz")
+        self.assertEqual("tar xjf test.tbz", cmd)
+
+        cmd = ft.extract_cmd("test.tar.bz2")
+        self.assertEqual("tar xjf test.tar.bz2", cmd)
+
+    def test_run_cmd(self):
+        """Basic test for run_cmd function."""
         (out, ec) = ft.run_cmd("echo hello")
         self.assertEqual(out, "hello\n")
         # no reason echo hello could fail
         self.assertEqual(ec, 0)
 
-        (out, ec) = ft.run_cmd_qa("echo question", {"question": "answer"})
-        self.assertEqual(out, "question\n")
+    def test_run_cmd_bis(self):
+        """More 'complex' test for run_cmd function."""
+        # a more 'complex' command to run, make sure all required output is there
+        (out, ec) = ft.run_cmd("for j in `seq 1 3`; do for i in `seq 1 100`; do echo hello; done; sleep 1.4; done")
+        self.assertTrue(out.startswith('hello\nhello\n'))
+        self.assertEqual(len(out), len("hello\n" * 300))
+        self.assertEqual(ec, 0)
+
+    def test_run_cmd_qa(self):
+        """Basic test for run_cmd_qa function."""
+        (out, ec) = ft.run_cmd_qa("echo question; read x; echo $x", {"question": "answer"})
+        self.assertEqual(out, "question\nanswer\n")
         # no reason echo hello could fail
         self.assertEqual(ec, 0)
 
+    def test_run_cmd_simple(self):
+        """Test return value for run_cmd in 'simple' mode."""
         self.assertEqual(True, ft.run_cmd("echo hello", simple=True))
         self.assertEqual(False, ft.run_cmd("exit 1", simple=True, log_all=False, log_ok=False))
 
+    def test_convert_name(self):
+        """Test convert_name function."""
         name = ft.convert_name("test+test-test")
         self.assertEqual(name, "testplustestmintest")
         name = ft.convert_name("test+test-test", True)
         self.assertEqual(name, "TESTPLUSTESTMINTEST")
 
+    def test_parse_log_error(self):
+        """Test basic parse_log_for_error functionality."""
         errors = ft.parse_log_for_error("error failed", True)
         self.assertEqual(len(errors), 1)
 
         # I expect tests to be run from the base easybuild directory
         self.assertEqual(os.getcwd(), ft.find_base_dir())
+
+    def test_run_cmd_suse(self):
+        """Test run_cmd on SuSE systems, which have $PROFILEREAD set."""
+        # avoid warning messages
+        ft_log_level = ft._log.getEffectiveLevel()
+        ft._log.setLevel('ERROR')
+
+        # run_cmd should also work if $PROFILEREAD is set (very relevant for SuSE systems)
+        profileread = os.environ.get('PROFILEREAD', None)
+        os.environ['PROFILEREAD'] = 'profilereadxxx'
+        try:
+            (out, ec) = ft.run_cmd("echo hello")
+        except Exception, err:
+            out, ec = "ERROR: %s" % err, 1
+
+        # make sure it's restored again before we can fail the test
+        if profileread is not None:
+            os.environ['PROFILEREAD'] = profileread
+        else:
+            del os.environ['PROFILEREAD']
+
+        self.assertEqual(out, "hello\n")
+        self.assertEqual(ec, 0)
+        ft._log.setLevel(ft_log_level)
+
+    def test_encode_class_name(self):
+        """Test encoding of class names."""
+        for (class_name, encoded_class_name) in self.CLASS_NAMES:
+            self.assertEqual(ft.encode_class_name(class_name), encoded_class_name)
+            self.assertEqual(ft.encode_class_name(ft.decode_class_name(encoded_class_name)), encoded_class_name)
+
+    def test_decode_class_name(self):
+        """Test decoding of class names."""
+        for (class_name, encoded_class_name) in self.CLASS_NAMES:
+            self.assertEqual(ft.decode_class_name(encoded_class_name), class_name)
+            self.assertEqual(ft.decode_class_name(ft.encode_class_name(class_name)), class_name)
+
+    def test_patch_perl_script_autoflush(self):
+        """Test patching Perl script for autoflush."""
+
+        fh, fp = tempfile.mkstemp()
+        os.close(fh)
+        perl_lines = [
+            "$!/usr/bin/perl",
+            "use strict;",
+            "print hello",
+            "",
+            "print hello again",
+        ]
+        perltxt = '\n'.join(perl_lines)
+        ft.write_file(fp, perltxt)
+        ft.patch_perl_script_autoflush(fp)
+        txt = ft.read_file(fp)
+        self.assertTrue(len(txt.split('\n')) == len(perl_lines) + 4)
+        self.assertTrue(txt.startswith(perl_lines[0] + "\n\nuse IO::Handle qw();\nSTDOUT->autoflush(1);"))
+        for line in perl_lines[1:]:
+            self.assertTrue(line in txt)
 
     def test_extract_tar(self):
         """Test the extraction of a tar file"""
