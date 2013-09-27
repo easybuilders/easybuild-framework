6029fd858125a8018bf4e42de3080eb6a4e7c310
Kenneth Hoste <kenneth.hoste@ugent.be>
2013-09-27 10:43:07 +0200

    make sure we have a useable logger for purge()

--- test/framework/options.py
+++ test/framework/options.py
@@ -274,7 +274,10 @@ class CommandLineOptionsTest(TestCase):
         # cleanup for next test
         write_file(self.logfile, '')
         os.chdir(pwd)
-        modules_tool().purge()
+        m = modules_tool()
+        # make the logger still works after stuff has been cleaned up by main
+        m.log = fancylogger.getLogger(self.__class__.__name__)
+        m.purge()
 
         # check log message with --skip for non-existing module
         args = [
