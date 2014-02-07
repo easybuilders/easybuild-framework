bf9a934cce3695275f20b5a8d869cdaa62617692
Kenneth Hoste <kenneth.hoste@ugent.be>
2014-02-07 13:28:20 +0100

    fix unit test w.r.t. parsing dependencies in format v2

--- test/framework/easyconfigparser.py
+++ test/framework/easyconfigparser.py
@@ -32,7 +32,8 @@ from unittest import TestCase, TestLoader, main
 from vsc.utils.fancylogger import setLogLevelDebug, logToScreen
 
 import easybuild.tools.build_log
-from easybuild.framework.easyconfig.format.version import EasyVersion
+from easybuild.framework.easyconfig.format.format import Dependency
+from easybuild.framework.easyconfig.format.version import EasyVersion, ToolchainVersionOperator, VersionOperator
 from easybuild.framework.easyconfig.parser import EasyConfigParser
 
 
@@ -113,7 +114,10 @@ class EasyConfigParserTest(TestCase):
         self.assertEqual(ec['toolchain'], {'name': 'goolf', 'version': '1.4.10'})
 
         # dependencies should be parsed correctly
-        self.assertEqual(ec['dependencies'], [{'name': 'zlib', 'version': '1.2.5'}])
+        deps = ec['dependencies']
+        self.assertTrue(isinstance(deps[0], Dependency))
+        self.assertEqual(deps[0].name(), 'zlib')
+        self.assertEqual(deps[0].version(), '1.2.5')
 
         fn = os.path.join(TESTDIRBASE, 'v2.0', 'goolf.eb')
         ecp = EasyConfigParser(fn)
@@ -125,16 +129,18 @@ class EasyConfigParserTest(TestCase):
 
         # dependencies should be parsed correctly
         deps = [
-            {'name': 'GCC', 'version': '4.7.2'},
-            {'name': 'OpenMPI', 'version': '1.6.4', 'toolchain': {'name': 'GCC', 'version': '4.7.2'}},
-            {'name': 'OpenBLAS', 'version': '0.2.6', 'versionsuffix': '-LAPACK-3.4.2',
-             'toolchain': {'name': 'gompi', 'version': '1.4.10'}},
-            {'name': 'FFTW', 'version': '3.3.3', 'toolchain': {'name': 'gompi', 'version': '1.4.10'}},
-            {'name': 'ScaLAPACK', 'version': '2.0.2', 'versionsuffix': '-OpenBLAS-0.2.6-LAPACK-3.4.2',
-             'toolchain': {'name': 'gompi', 'version': '1.4.10'}},
+            # name, version, versionsuffix, toolchain
+            ('GCC', '4.7.2', None, None),
+            ('OpenMPI', '1.6.4', None, {'name': 'GCC', 'version': '4.7.2'}),
+            ('OpenBLAS', '0.2.6', '-LAPACK-3.4.2', {'name': 'gompi', 'version': '1.4.10'}),
+            ('FFTW', '3.3.3', None, {'name': 'gompi', 'version': '1.4.10'}),
+            ('ScaLAPACK', '2.0.2', '-OpenBLAS-0.2.6-LAPACK-3.4.2', {'name': 'gompi', 'version': '1.4.10'}),
         ]
-        self.assertEqual(ec['dependencies'], deps)
-
+        for i, (name, version, versionsuffix, toolchain) in enumerate(deps):
+            self.assertEqual(ec['dependencies'][i].name(), name)
+            self.assertEqual(ec['dependencies'][i].version(), version)
+            self.assertEqual(ec['dependencies'][i].versionsuffix(), versionsuffix)
+            self.assertEqual(ec['dependencies'][i].toolchain(), toolchain)
 
         # restore
         easybuild.tools.build_log.EXPERIMENTAL = orig_experimental
