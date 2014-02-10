85c373e6f381cdd137bee21bcfaa960ef80fe814
stdweird <stijn.deweirdt@ugent.be>
2014-02-10 13:56:55 +0100

    move xml code to jenkins module

--- easybuild/tools/filetools.py
+++ easybuild/tools/filetools.py
@@ -1,4 +1,4 @@
-##
+# #
 # Copyright 2009-2014 Ghent University
 #
 # This file is part of EasyBuild,
@@ -21,7 +21,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with EasyBuild.  If not, see <http://www.gnu.org/licenses/>.
-##
+# #
 """
 Set of file tools.
 
@@ -34,7 +34,6 @@ Set of file tools.
 @author: Ward Poelmans (Ghent University)
 """
 import errno
-import glob
 import os
 import re
 import shutil
@@ -44,16 +43,13 @@ import subprocess
 import tempfile
 import time
 import urllib
-import xml.dom.minidom as xml
 import zlib
-from datetime import datetime
 from vsc import fancylogger
 from vsc.utils.missing import all
 
 import easybuild.tools.environment as env
 from easybuild.tools.asyncprocess import PIPE, STDOUT, Popen, recv_some, send_all
 from easybuild.tools.build_log import print_msg  # import build_log must stay, to activate use of EasyBuildLog
-from easybuild.tools.version import FRAMEWORK_VERSION, EASYBLOCKS_VERSION
 
 
 _log = fancylogger.getLogger('filetools', fname=False)
@@ -192,16 +188,16 @@ def extract_file(fn, dest, cmd=None, extra_options=None, overwrite=False):
         _log.error("Can't extract file %s: no such file" % fn)
 
     if not os.path.isdir(dest):
-        ## try to create it
+        # # try to create it
         try:
             os.makedirs(dest)
         except OSError, err:
             _log.exception("Can't extract file %s: directory %s can't be created: %err " % (fn, dest, err))
 
-    ## use absolute pathnames from now on
+    # # use absolute pathnames from now on
     absDest = os.path.abspath(dest)
 
-    ## change working directory
+    # # change working directory
     try:
         _log.debug("Unpacking %s in directory %s." % (fn, absDest))
         os.chdir(absDest)
@@ -459,7 +455,7 @@ def find_base_dir():
       expect only the first one to give the correct path
     """
     def get_local_dirs_purged():
-        ## e.g. always purge the log directory
+        # # e.g. always purge the log directory
         ignoreDirs = ["easybuild"]
 
         lst = os.listdir(os.getcwd())
@@ -558,7 +554,7 @@ def apply_patch(patchFile, dest, fn=None, copy=False, level=None):
         _log.error("Can't patch directory %s: no such directory" % dest)
         return
 
-    ## copy missing files
+    # # copy missing files
     if copy:
         try:
             shutil.copy2(patchFile, dest)
@@ -568,7 +564,7 @@ def apply_patch(patchFile, dest, fn=None, copy=False, level=None):
             _log.error("Failed to copy %s to dir %s: %s" % (patchFile, dest, err))
             return
 
-    ## use absolute paths
+    # # use absolute paths
     apatch = os.path.abspath(patchFile)
     adest = os.path.abspath(dest)
 
@@ -605,7 +601,7 @@ def apply_patch(patchFile, dest, fn=None, copy=False, level=None):
 
         p = None
         for line in plusLines:
-            ## locate file by stripping of /
+            # # locate file by stripping of /
             f = line.group('file')
             tf2 = f.split('/')
             n = len(tf2)
@@ -683,7 +679,7 @@ def run_cmd(cmd, log_ok=True, log_all=False, simple=False, inp=None, regexp=True
     except:
         _log.info("running cmd %s in non-existing directory, might fail!" % cmd)
 
-    ## Log command output
+    # # Log command output
     if log_output:
         runLog = tempfile.NamedTemporaryFile(suffix='.log', prefix='easybuild-run_cmd-')
         _log.debug('run_cmd: Command output will be logged to %s' % runLog.name)
@@ -719,7 +715,7 @@ def run_cmd(cmd, log_ok=True, log_all=False, simple=False, inp=None, regexp=True
     # not needed anymore. subprocess does this correct?
     # ec=os.WEXITSTATUS(ec)
 
-    ## Command log output
+    # # Command log output
     if log_output:
         runLog.close()
 
@@ -763,7 +759,7 @@ def run_cmd_qa(cmd, qa, no_qa=None, log_ok=True, log_all=False, simple=False, re
     def process_QA(q, a):
         splitq = [escape_special(x) for x in regSplit.split(q)]
         regQtxt = split.join(splitq) + split.rstrip('+') + "*$"
-        ## add optional split at the end
+        # # add optional split at the end
         if not a.endswith('\n'):
             a += '\n'
         regQ = re.compile(r"" + regQtxt)
@@ -798,7 +794,7 @@ def run_cmd_qa(cmd, qa, no_qa=None, log_ok=True, log_all=False, simple=False, re
     # Part 2: Run the command and answer questions
     # - this needs asynchronous stdout
 
-    ## Log command output
+    # # Log command output
     if log_all:
         try:
             runLog = tempfile.NamedTemporaryFile(suffix='.log', prefix='easybuild-cmdqa-')
@@ -965,7 +961,7 @@ def convert_name(name, upper=False):
     """
     Converts name so it can be used as variable name
     """
-    ## no regexps
+    # # no regexps
     charmap = {
         '+': 'plus',
         '-': 'min'
@@ -1199,7 +1195,7 @@ def copytree(src, dst, symlinks=False, ignore=None):
     class Error(EnvironmentError):
         pass
     try:
-        WindowsError  #@UndefinedVariable
+        WindowsError  # @UndefinedVariable
     except NameError:
         WindowsError = None
 
@@ -1288,133 +1284,3 @@ def decode_class_name(name):
     else:
         name = name[len(EASYBLOCK_CLASS_PREFIX):]
         return decode_string(name)
-
-
-def write_to_xml(succes, failed, filename):
-    """
-    Create xml output, using minimal output required according to
-    http://stackoverflow.com/questions/4922867/junit-xml-format-specification-that-hudson-supports
-    """
-    dom = xml.getDOMImplementation()
-    root = dom.createDocument(None, "testsuite", None)
-
-    def create_testcase(name):
-        el = root.createElement("testcase")
-        el.setAttribute("name", name)
-        return el
-
-    def create_failure(name, error_type, error):
-        el = create_testcase(name)
-
-        # encapsulate in CDATA section
-        error_text = root.createCDATASection("\n%s\n" % error)
-        failure_el = root.createElement("failure")
-        failure_el.setAttribute("type", error_type)
-        el.appendChild(failure_el)
-        el.lastChild.appendChild(error_text)
-        return el
-
-    def create_success(name, stats):
-        el = create_testcase(name)
-        text = "\n".join(["%s=%s" % (key, value) for (key, value) in stats.items()])
-        build_stats = root.createCDATASection("\n%s\n" % text)
-        system_out = root.createElement("system-out")
-        el.appendChild(system_out)
-        el.lastChild.appendChild(build_stats)
-        return el
-
-    properties = root.createElement("properties")
-    framework_version = root.createElement("property")
-    framework_version.setAttribute("name", "easybuild-framework-version")
-    framework_version.setAttribute("value", str(FRAMEWORK_VERSION))
-    properties.appendChild(framework_version)
-    easyblocks_version = root.createElement("property")
-    easyblocks_version.setAttribute("name", "easybuild-easyblocks-version")
-    easyblocks_version.setAttribute("value", str(EASYBLOCKS_VERSION))
-    properties.appendChild(easyblocks_version)
-
-    time = root.createElement("property")
-    time.setAttribute("name", "timestamp")
-    time.setAttribute("value", str(datetime.now()))
-    properties.appendChild(time)
-
-    root.firstChild.appendChild(properties)
-
-    for (obj, fase, error, _) in failed:
-        # try to pretty print
-        try:
-            el = create_failure(obj.mod_name, fase, error)
-        except AttributeError:
-            el = create_failure(obj, fase, error)
-
-        root.firstChild.appendChild(el)
-
-    for (obj, stats) in succes:
-        el = create_success(obj.mod_name, stats)
-        root.firstChild.appendChild(el)
-
-    try:
-        output_file = open(filename, "w")
-        root.writexml(output_file)
-        output_file.close()
-    except IOError, err:
-        _log.error("Failed to write out XML file %s: %s" % (filename, err))
-
-
-def aggregate_xml_in_dirs(base_dir, output_filename):
-    """
-    Finds all the xml files in the dirs and takes the testcase attribute out of them.
-    These are then put in a single output file.
-    """
-    dom = xml.getDOMImplementation()
-    root = dom.createDocument(None, "testsuite", None)
-    root.documentElement.setAttribute("name", base_dir)
-    properties = root.createElement("properties")
-    framework_version = root.createElement("property")
-    framework_version.setAttribute("name", "easybuild-framework-version")
-    framework_version.setAttribute("value", str(FRAMEWORK_VERSION))
-    properties.appendChild(framework_version)
-    easyblocks_version = root.createElement("property")
-    easyblocks_version.setAttribute("name", "easybuild-easyblocks-version")
-    easyblocks_version.setAttribute("value", str(EASYBLOCKS_VERSION))
-    properties.appendChild(easyblocks_version)
-
-    time_el = root.createElement("property")
-    time_el.setAttribute("name", "timestamp")
-    time_el.setAttribute("value", str(datetime.now()))
-    properties.appendChild(time_el)
-
-    root.firstChild.appendChild(properties)
-
-    dirs = filter(os.path.isdir, [os.path.join(base_dir, d) for d in os.listdir(base_dir)])
-
-    succes = 0
-    total = 0
-
-    for d in dirs:
-        xml_file = glob.glob(os.path.join(d, "*.xml"))
-        if xml_file:
-            # take the first one (should be only one present)
-            xml_file = xml_file[0]
-            try:
-                dom = xml.parse(xml_file)
-            except IOError, err:
-                _log.error("Failed to read/parse XML file %s: %s" % (xml_file, err))
-            # only one should be present, we are just discarding the rest
-            testcase = dom.getElementsByTagName("testcase")[0]
-            root.firstChild.appendChild(testcase)
-
-            total += 1
-            if not testcase.getElementsByTagName("failure"):
-                succes += 1
-
-    comment = root.createComment("%s out of %s builds succeeded" % (succes, total))
-    root.firstChild.insertBefore(comment, properties)
-    try:
-        output_file = open(output_filename, "w")
-        root.writexml(output_file, addindent="\t", newl="\n")
-        output_file.close()
-    except IOError, err:
-        _log.error("Failed to write out XML file %s: %s" % (output_filename, err))
-
-    print "Aggregate regtest results written to %s" % output_filename
