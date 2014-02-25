8b0d4caef94d68fc33ccf0e8b2458243876c6123
Kenneth Hoste <kenneth.hoste@ugent.be>
2014-02-25 14:36:47 +0100

    add unit test for module function vs modules tool mismatch check

--- test/framework/modulestool.py
+++ test/framework/modulestool.py
@@ -28,12 +28,16 @@ Unit tests for ModulesTool class.
 @author: Stijn De Weirdt (Ghent University)
 """
 import os
+import re
+import tempfile
+from vsc import fancylogger
 
 from unittest import main as unittestmain
 from unittest import TestCase, TestLoader
 from distutils.version import StrictVersion
 
-from easybuild.tools import modules
+import easybuild.tools.options as eboptions
+from easybuild.tools import config, modules
 from easybuild.tools.build_log import EasyBuildError
 from easybuild.tools.filetools import which
 
@@ -56,8 +60,39 @@ class BrokenMockModulesTool(MockModulesTool):
 class ModulesToolTest(TestCase):
     """ Testcase for ModulesTool """
 
+    def assertErrorRegex(self, error, regex, call, *args):
+        """ convenience method to match regex with the error message """
+        try:
+            call(*args)
+        except error, err:
+            self.assertTrue(re.search(regex, err.msg))
+
+    def setUp(self):
+        """Testcase setup."""
+        # initialize configuration so config.get_modules_tool function works
+        eb_go = eboptions.parse_options()
+        config.init(eb_go.options, eb_go.get_options_by_section('config'))
+
+        fd, self.log_fn = tempfile.mkstemp(prefix='easybuild-tests-modules-tool-', suffix='.log')
+        os.close(fd)
+        os.remove(self.log_fn)
+        fancylogger.logToFile(self.log_fn)
+
+        # keep track of original 'module' function definition so we can restore it
+        self.orig_module = os.environ['module']
+
+    def tearDown(self):
+        """Testcase cleanup."""
+        fancylogger.logToFile(self.log_fn, enable=False)
+        os.remove(self.log_fn)
+
+        # restore
+        os.environ['module'] = self.orig_module
+
     def test_mock(self):
         """Test the mock module"""
+        os.environ['module'] = "() {  eval `/bin/echo $*`\n}"
+
         # ue empty mod_path list, otherwise the install_path is called
         mmt = MockModulesTool(mod_paths=[])
 
@@ -71,6 +106,7 @@ class ModulesToolTest(TestCase):
 
     def test_environment_command(self):
         """Test setting cmd via enviroment"""
+        os.environ['module'] = "() { %s $*\n}" % BrokenMockModulesTool.COMMAND
 
         try:
             bmmt = BrokenMockModulesTool(mod_paths=[])
@@ -80,6 +116,7 @@ class ModulesToolTest(TestCase):
             self.assertTrue('command is not available' in str(err))
 
         os.environ[BrokenMockModulesTool.COMMAND_ENVIRONMENT] = MockModulesTool.COMMAND
+        os.environ['module'] = "() { /bin/echo $*\n}"
         bmmt = BrokenMockModulesTool(mod_paths=[])
         cmd_abspath = which(MockModulesTool.COMMAND)
 
@@ -89,6 +126,25 @@ class ModulesToolTest(TestCase):
         # clean it up
         del os.environ[BrokenMockModulesTool.COMMAND_ENVIRONMENT]
 
+    def test_module_mismatch(self):
+        """Test whether mismatch detection between modules tool and 'module' function works."""
+        # redefine 'module' function (deliberate mismatch with used module command in MockModulesTool)
+        os.environ['module'] = "() {  eval `/Users/kehoste/Modules/$MODULE_VERSION/bin/modulecmd bash $*`\n}"
+        self.assertErrorRegex(EasyBuildError, ".*command .* not found in defined 'module' function", MockModulesTool)
+
+        # redefine 'module' function with correct module command
+        os.environ['module'] = "() {  eval `/bin/echo $*`\n}"
+        mt = MockModulesTool()
+        self.assertTrue(isinstance(mt.loaded_modules(), list))  # dummy usage
+
+        # a warning should be logged if the 'module' function is undefined
+        del os.environ['module']
+        mt = MockModulesTool()
+        f = open(self.log_fn, 'r')
+        logtxt = f.read()
+        f.close()
+        warning_regex = re.compile("WARNING 'module' function not defined, can't verify whether modules tool matches it.")
+        self.assertTrue(warning_regex.search(logtxt))
 
 def suite():
     """ returns all the testcases in this module """
