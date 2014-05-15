09ddd629600573c06c4a6ba0d71d8be2822ce3f6
Kenneth Hoste <kenneth.hoste@ugent.be>
2014-05-15 10:40:29 +0200

    fix help message for --test-easyconfigs-pr

--- easybuild/tools/options.py
+++ easybuild/tools/options.py
@@ -282,7 +282,8 @@ class EasyBuildOptions(GeneralOption):
                                    None, 'store', None, {'metavar': 'DIR'}),
             'sequential': ("Specify this option if you want to prevent parallel build",
                            None, 'store_true', False),
-            'test-easyconfigs-pr': ("Test easyconfigs from specified PR", int, 'store', None, {'metavar': 'PR#'}),
+            'test-easyconfigs-pr': ("Test easyconfigs from specified PR, and submit a test report",
+                                    int, 'store', None, {'metavar': 'PR#'}),
         })
 
         self.log.debug("regtest_options: descr %s opts %s" % (descr, opts))
