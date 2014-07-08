d8377195152cbd772ba23bc091fdbf239fb969dd
Kenneth Hoste <kenneth.hoste@ugent.be>
2014-07-08 14:03:49 +0200

    move ModuleNamingScheme class into dedicated mns.py module

--- easybuild/tools/module_naming_scheme/__init__.py
+++ easybuild/tools/module_naming_scheme/__init__.py
@@ -28,10 +28,10 @@ Declares easybuild.tools.module_naming_scheme namespace, in an extendable way.
 @author: Jens Timmerman (Ghent University)
 @author: Kenneth Hoste (Ghent University)
 """
-import os
 from pkgutil import extend_path
-from vsc.utils import fancylogger
-from vsc.utils.patterns import Singleton
+
+# required for backward compatibility
+from easybuild.tools.module_naming_scheme.mns import ModuleNamingScheme
 
 # we're not the only ones in this namespace
 __path__ = extend_path(__path__, __name__)  #@ReservedAssignment
@@ -41,89 +41,3 @@ DEVEL_MODULE_SUFFIX = '-easybuild-devel'
 
 # general module class
 GENERAL_CLASS = 'all'
-
-
-class ModuleNamingScheme(object):
-    """Abstract class for a module naming scheme implementation."""
-
-    # singleton metaclass: only one instance is created
-    __metaclass__ = Singleton
-
-    REQUIRED_KEYS = None
-
-    def __init__(self, *args, **kwargs):
-        """Initialize logger."""
-        self.log = fancylogger.getLogger(self.__class__.__name__, fname=False)
-
-    def is_sufficient(self, keys):
-        """Determine whether specified list of easyconfig parameters is sufficient for this module naming scheme."""
-        if self.REQUIRED_KEYS is not None:
-            return set(keys).issuperset(set(self.REQUIRED_KEYS))
-        else:
-            self.log.error("Constant REQUIRED_KEYS is not defined, should specify required easyconfig parameters.")
-
-    def requires_toolchain_details(self):
-        """
-        Determine whether toolchain details are required by this module naming scheme,
-        e.g. whether one of det_toolchain_* functions are relied upon.
-        """
-        return False
-
-    def det_full_module_name(self, ec):
-        """
-        Determine full module name, relative to the top of the module path.
-
-        @param ec: dict-like object with easyconfig parameter values; for now only the 'name',
-                   'version', 'versionsuffix' and 'toolchain' parameters are guaranteed to be available
-
-        @return: string with full module name, e.g.: '<compiler>/<mpi_lib>/<name>/<version>'
-        """
-        raise NotImplementedError
-
-    def det_short_module_name(self, ec):
-        """
-        Determine short module name, i.e. the name under which modules will be exposed to users.
-
-        @param ec: dict-like object with easyconfig parameter values; for now only the 'name',
-                   'version', 'versionsuffix' and 'toolchain' parameters are guaranteed to be available
-        @return: string with module name, e.g. '<name>/<version>'
-        """
-        # by default: full module name doesn't include a $MODULEPATH subdir
-        return self.det_full_module_name(ec)
-
-    def det_module_subdir(self, ec):
-        """
-        Determine subdirectory for module file in $MODULEPATH.
-        This determines the separation between module names exposed to users, and what's part of the $MODULEPATH.
-
-        @param ec: dict-like object with easyconfig parameter values; for now only the 'name',
-                   'version', 'versionsuffix' and 'toolchain' parameters are guaranteed to be available
-        @return: string with subdir path (relative to $MODULEPATH), e.g. '<compiler>/<mpi_lib>'
-        """
-        # by default: no subdirectory
-        return ''
-
-    def det_modpath_extensions(self, ec):
-        """
-        Determine list of subdirectories for which to extend $MODULEPATH with when this module is loaded (if any).
-
-        @param ec: dict-like object with easyconfig parameter values; for now only the 'name',
-                   'version', 'versionsuffix' and 'toolchain' parameters are guaranteed to be available
-        @return: A list of $MODULEPATH subdirectories.
-        """
-        # by default: an empty list of subdirectories to extend $MODULEPATH with
-        return []
-
-    def det_init_modulepaths(self, ec):
-        """
-        Determine initial module paths, where the modules that are top of the hierarchy (if any) live.
-        """
-        return []
-
-    def expand_toolchain_load(self):
-        """
-        Determine whether load statements for a toolchain should be expanded to load statements for its dependencies.
-        This is useful when toolchains are not exposed to users.
-        """
-        # by default: just include a load statement for the toolchain
-        return False
