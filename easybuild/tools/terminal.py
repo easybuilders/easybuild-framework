bdc096c43d7cf8a946f70dc27e1b2d668dab9cce
Toon Willems <willemstoon@gmail.com>
2014-08-14 10:40:54 +0200

    add header, note about failing imports

--- easybuild/tools/terminal.py
+++ easybuild/tools/terminal.py
@@ -1,10 +1,40 @@
-# http://stackoverflow.com/questions/566746/how-to-get-console-window-width-in-python
+#!/usr/bin/env python
+# #
+# Copyright 2009-2014 Ghent University
+#
+# This file is part of EasyBuild,
+# originally created by the HPC team of Ghent University (http://ugent.be/hpc/en),
+# with support of Ghent University (http://ugent.be/hpc),
+# the Flemish Supercomputer Centre (VSC) (https://vscentrum.be/nl/en),
+# the Hercules foundation (http://www.herculesstichting.be/in_English)
+# and the Department of Economy, Science and Innovation (EWI) (http://www.ewi-vlaanderen.be/en).
+#
+# http://github.com/hpcugent/easybuild
+#
+# EasyBuild is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation v2.
+#
+# EasyBuild is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with EasyBuild.  If not, see <http://www.gnu.org/licenses/>.
+# #
+"""
+Module for checking the terminal dimensions
+copied from http://stackoverflow.com/questions/566746/how-to-get-console-window-width-in-python
+"""
+
 import os
 
 def get_terminal_size():
     env = os.environ
     def ioctl_GWINSZ(fd):
         try:
+            # these might fail because they are only available on Unix
             import fcntl, termios, struct, os
             cr = struct.unpack('hh', fcntl.ioctl(fd, termios.TIOCGWINSZ,
         '1234'))
@@ -22,9 +52,4 @@ def get_terminal_size():
     if not cr:
         cr = (env.get('LINES', 25), env.get('COLUMNS', 80))
 
-        ### Use get(key[, default]) instead of a try/catch
-        #try:
-        #    cr = (env['LINES'], env['COLUMNS'])
-        #except:
-        #    cr = (25, 80)
     return int(cr[1]), int(cr[0])
