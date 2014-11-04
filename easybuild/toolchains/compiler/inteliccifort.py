78f158b2614f3c83bdda0a00010808ea34ebb99a
pescobar <pescobar001@gmail.com>
2014-11-04 10:41:46 +0100

    switched from mavx to xHost

--- easybuild/toolchains/compiler/inteliccifort.py
+++ easybuild/toolchains/compiler/inteliccifort.py
@@ -70,7 +70,7 @@ class IntelIccIfort(Compiler):
 
     COMPILER_OPTIMAL_ARCHITECTURE_OPTION = {
         systemtools.INTEL : 'xHOST',
-        systemtools.AMD : 'mavx',
+        systemtools.AMD : 'xHost',
     }
 
     COMPILER_CC = 'icc'
