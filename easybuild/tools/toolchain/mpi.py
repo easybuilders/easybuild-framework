29466b1dfcc452561f0ec55020c4866c138848ce
ebgregory <e.gregory@fz-juelich.de>
2014-11-20 17:25:53 +0100

    adding support for MPICH (instead of just MPICH2)

--- easybuild/tools/toolchain/mpi.py
+++ easybuild/tools/toolchain/mpi.py
@@ -176,6 +176,7 @@ class Mpi(Toolchain):
             toolchain.INTELMPI: "mpirun %(mpdbf)s %(nodesfile)s -np %(nr_ranks)d %(cmd)s",  # @UndefinedVariable
             toolchain.MVAPICH2: "mpirun -n %(nr_ranks)d %(cmd)s",  # @UndefinedVariable
             toolchain.MPICH2: "mpirun -n %(nr_ranks)d %(cmd)s",  # @UndefinedVariable
+            toolchain.MPICH: "mpirun -n %(nr_ranks)d %(cmd)s",  # @UndefinedVariable
         }
 
         mpi_family = self.mpi_family()
