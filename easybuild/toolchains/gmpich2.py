439a1546df91378c1ceab9c00f0dc7628e011114
Markus Geimer <m.geimer@fz-juelich.de>
2014-12-02 20:36:15 +0100

    Re-added gmpich2 toolchain

---
+++ easybuild/toolchains/gmpich2.py
@@ -0,0 +1,37 @@
+##
+# Copyright 2012-2014 Ghent University
+#
+# This file is part of EasyBuild,
+# originally created by the HPC team of Ghent University (http://ugent.be/hpc/en),
+# with support of Ghent University (http://ugent.be/hpc),
+# the Flemish Supercomputer Centre (VSC) (https://vscentrum.be/nl/en),
+# the Hercules foundation (http://www.herculesstichting.be/in_English)
+# and the Department of Economy, Science and Innovation (EWI) (http://www.ewi-vlaanderen.be/en).
+#
+# http://github.com/hpcugent/easybuild
+#
+# EasyBuild is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation v2.
+#
+# EasyBuild is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with EasyBuild.  If not, see <http://www.gnu.org/licenses/>.
+##
+"""
+EasyBuild support for gmpich2 compiler toolchain (includes GCC and MPICH2).
+
+@author: Kenneth Hoste (Ghent University)
+"""
+
+from easybuild.toolchains.compiler.gcc import Gcc
+from easybuild.toolchains.mpi.mpich2 import Mpich2
+
+
+class Gmpich2(Gcc, Mpich2):
+    """Compiler toolchain with GCC and MPICH2."""
+    NAME = 'gmpich2'
