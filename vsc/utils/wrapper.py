4afe0a9ea101f2af32acf78466b96b67bc0a84ff
Kenneth Hoste <kenneth.hoste@ugent.be>
2014-12-13 20:36:48 +0100

    update with vsc-base for HybridListDict

--- vsc/utils/wrapper.py
+++ vsc/utils/wrapper.py
@@ -40,3 +40,45 @@ class Wrapper(object):
                     if name.startswith("__"):
                         if name not in ignore and name not in dct:
                             setattr(cls, name, property(make_proxy(name)))
+
+
+class HybridListDict(Wrapper):
+    """
+    Hybrid list/dict object: is a list of 2-element tuples, but also acts like a dict.
+
+    Supported dict-like methods include: update(adict), items(), keys(), values()
+    """
+    __wraps__ = list
+
+    def __getitem__(self, index_key):
+        """Get value by specified index/key."""
+        if isinstance(index_key, int):
+            res = self._obj[index_key]
+        else:
+            res = dict(self._obj)[index_key]
+        return res
+
+    def __setitem__(self, index_key, value):
+        """Add value at specified index/key."""
+        if isinstance(index_key, int):
+            self._obj[index_key] = value
+        else:
+            self._obj = [(k, v) for (k, v) in self._obj if k != index_key]
+            self._obj.append((index_key, value))
+
+    def update(self, extra):
+        """Update with keys/values in supplied dictionary."""
+        self._obj = [(k, v) for (k, v) in self._obj if k not in extra.keys()]
+        self._obj.extend(extra.items())
+
+    def items(self):
+        """Get list of key/value tuples."""
+        return self._obj
+
+    def keys(self):
+        """Get list of keys."""
+        return [x[0] for x in self.items()]
+
+    def values(self):
+        """Get list of values."""
+        return [x[1] for x in self.items()]
