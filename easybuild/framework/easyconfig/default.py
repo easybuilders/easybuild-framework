bf1746703d9a0b3c394e94118e4fa88cbc7678ec
Kenneth Hoste <kenneth.hoste@ugent.be>
2015-01-16 22:22:21 +0100

    cleaner solution for defining category constants in default.py

--- easybuild/framework/easyconfig/default.py
+++ easybuild/framework/easyconfig/default.py
@@ -40,31 +40,23 @@ _log = fancylogger.getLogger('easyconfig.default', fname=False)
 
 
 # we use a tuple here so we can sort them based on the numbers
-HIDDEN = (-1, 'hidden')
-MANDATORY = (0, 'mandatory')
-CUSTOM = (1, 'easyblock-specific')
-TOOLCHAIN = (2, 'toolchain')
-BUILD = (3, 'build')
-FILEMANAGEMENT = (4, 'file-management')
-DEPENDENCIES = (5, 'dependencies')
-LICENSE = (6, 'license')
-EXTENSIONS = (7, 'extensions')
-MODULES = (8, 'modules')
-OTHER = (9, 'other')
-
 ALL_CATEGORIES = {
-    'HIDDEN': HIDDEN,
-    'MANDATORY': MANDATORY,
-    'CUSTOM': CUSTOM,
-    'TOOLCHAIN': TOOLCHAIN,
-    'BUILD': BUILD,
-    'FILEMANAGEMENT': FILEMANAGEMENT,
-    'DEPENDENCIES': DEPENDENCIES,
-    'LICENSE': LICENSE,
-    'EXTENSIONS': EXTENSIONS,
-    'MODULES': MODULES,
-    'OTHER': OTHER,
+    'HIDDEN': (-1, 'hidden'),
+    'MANDATORY': (0, 'mandatory'),
+    'CUSTOM': (1, 'easyblock-specific'),
+    'TOOLCHAIN': (2, 'toolchain'),
+    'BUILD': (3, 'build'),
+    'FILEMANAGEMENT': (4, 'file-management'),
+    'DEPENDENCIES': (5, 'dependencies'),
+    'LICENSE': (6, 'license'),
+    'EXTENSIONS': (7, 'extensions'),
+    'MODULES': (8, 'modules'),
+    'OTHER': (9, 'other'),
 }
+# define constants so they can be used below
+# avoid that pylint complains about unknown variables in this file
+# pylint: disable=E0602
+globals().update(ALL_CATEGORIES)
 
 # List of tuples. Each tuple has the following format (key, [default, help text, category])
 DEFAULT_CONFIG = {
