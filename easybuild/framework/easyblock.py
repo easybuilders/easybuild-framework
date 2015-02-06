7247010347ca25e8bfda6df0722597a63ce41a28
Kenneth Hoste <kenneth.hoste@ugent.be>
2015-02-06 22:27:59 +0100

    make sure newspec exists before using os.path.samefile on it

--- easybuild/framework/easyblock.py
+++ easybuild/framework/easyblock.py
@@ -1936,7 +1936,7 @@ def build_and_install_one(module, orig_environ):
         try:
             newspec = os.path.join(new_log_dir, "%s-%s.eb" % (app.name, det_full_ec_version(app.cfg)))
             # only copy if the files are not the same actual file already
-            if not os.path.samefile(spec, newspec):
+            if not (os.path.exists(newspec) or os.path.samefile(spec, newspec)):
                 shutil.copy(spec, newspec)
             _log.debug("Copied easyconfig file %s to %s" % (spec, newspec))
         except (IOError, OSError), err:
