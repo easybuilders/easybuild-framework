a2c033660a91bfb43040d5988d80153c921398c7
Riccardo Murri <riccardo.murri@gmail.com>
2015-02-13 15:09:28 +0100

    gc3pie: Correctly capitalize GC3Pie in docstring.

--- easybuild/tools/job/gc3pie.py
+++ easybuild/tools/job/gc3pie.py
@@ -23,7 +23,7 @@
 # You should have received a copy of the GNU General Public License
 # along with EasyBuild.  If not, see <http://www.gnu.org/licenses/>.
 ##
-"""Interface for submitting jobs via gc3pie"""
+"""Interface for submitting jobs via GC3Pie"""
 
 
 import os
