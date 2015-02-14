cf6b6b86f8deb2bdf3a6622da801ab5257f04b52
Kenneth Hoste <kenneth.hoste@ugent.be>
2015-02-14 23:23:23 +0100

    fix broken test

--- test/framework/modulestool.py
+++ test/framework/modulestool.py
@@ -156,6 +156,7 @@ class ModulesToolTest(EnhancedTestCase):
         if lmod_abspath is not None:
             build_options = {
                 'allow_modules_tool_mismatch': True,
+                'update_lmod_caches': True,
             }
             init_config(build_options=build_options)
 
