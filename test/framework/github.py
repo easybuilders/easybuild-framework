3869d27b80239009e8aadba88478832cf55787b5
Kenneth Hoste <kenneth.hoste@ugent.be>
2015-02-20 22:52:01 +0100

    make test_fetch_easyconfigs_from_pr robust against offline testing

--- test/framework/github.py
+++ test/framework/github.py
@@ -33,6 +33,7 @@ import shutil
 import tempfile
 from test.framework.utilities import EnhancedTestCase
 from unittest import TestLoader, main
+from urllib2 import URLError
 
 from easybuild.tools.build_log import EasyBuildError
 from easybuild.tools.github import Githubfs, fetch_github_token, fetch_easyconfigs_from_pr
@@ -109,14 +110,19 @@ class GithubTest(EnhancedTestCase):
         # PR for ictce/6.2.5, see https://github.com/hpcugent/easybuild-easyconfigs/pull/726/files
         all_ecs = ['gzip-1.6-ictce-6.2.5.eb', 'icc-2013_sp1.2.144.eb', 'ictce-6.2.5.eb', 'ifort-2013_sp1.2.144.eb',
                    'imkl-11.1.2.144.eb', 'impi-4.1.3.049.eb']
-        ec_files = fetch_easyconfigs_from_pr(726, path=tmpdir, github_user=GITHUB_TEST_ACCOUNT)
-        self.assertEqual(all_ecs, sorted([os.path.basename(f) for f in ec_files]))
-        self.assertEqual(all_ecs, sorted(os.listdir(tmpdir)))
-        shutil.rmtree(tmpdir)
+        try:
+            ec_files = fetch_easyconfigs_from_pr(726, path=tmpdir, github_user=GITHUB_TEST_ACCOUNT)
+            self.assertEqual(all_ecs, sorted([os.path.basename(f) for f in ec_files]))
+            self.assertEqual(all_ecs, sorted(os.listdir(tmpdir)))
 
-        # PR for EasyBuild v1.13.0 release (250+ commits, 218 files changed)
-        err_msg = "PR #897 contains more than .* commits, can't obtain last commit"
-        self.assertErrorRegex(EasyBuildError, err_msg, fetch_easyconfigs_from_pr, 897, github_user=GITHUB_TEST_ACCOUNT)
+            # PR for EasyBuild v1.13.0 release (250+ commits, 218 files changed)
+            err_msg = "PR #897 contains more than .* commits, can't obtain last commit"
+            self.assertErrorRegex(EasyBuildError, err_msg, fetch_easyconfigs_from_pr, 897, github_user=GITHUB_TEST_ACCOUNT)
+
+        except URLError, err:
+            print "Ignoring URLError '%s' in test_fetch_easyconfigs_from_pr" % err
+
+        shutil.rmtree(tmpdir)
 
 
 def suite():
