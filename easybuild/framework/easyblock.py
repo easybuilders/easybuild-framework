c869aff75809133f0691a6c246107b2fb25a28c2
Petar Forai <pfo@pseudoterminal.org>
2015-03-17 23:29:14 +0100

    Initial support for non-EB generated system module files.

--- easybuild/framework/easyblock.py
+++ easybuild/framework/easyblock.py
@@ -1305,7 +1305,8 @@ class EasyBlock(object):
         Pre-configure step. Set's up the builddir just before starting configure
         """
         self.cfg['unwanted_env_vars'] = env.unset_env_vars(self.cfg['unwanted_env_vars'])
-        self.toolchain.prepare(self.cfg['onlytcmod'])
+        self.modules_tool.load(self.cfg['system_modules']) 
+	self.toolchain.prepare(self.cfg['onlytcmod'])
         self.guess_start_dir()
 
     def configure_step(self):
