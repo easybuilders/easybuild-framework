a4c78b6b7213db4b05f99a32a1431e43a29827d6
Kenneth Hoste <kenneth.hoste@ugent.be>
2015-06-09 11:20:23 +0200

    add unit test for GC3Pie

--- test/framework/parallelbuild.py
+++ test/framework/parallelbuild.py
@@ -33,12 +33,28 @@ from unittest import TestLoader, main
 from vsc.utils.fancylogger import setLogLevelDebug, logToScreen
 
 from easybuild.framework.easyconfig.tools import process_easyconfig
-from easybuild.tools import config, job
+from easybuild.tools import config
+from easybuild.tools.filetools import write_file
 from easybuild.tools.job import pbs_python
 from easybuild.tools.job.pbs_python import PbsPython
 from easybuild.tools.parallelbuild import build_easyconfigs_in_parallel
 from easybuild.tools.robot import resolve_dependencies
 
+
+GC3PIE_LOCAL_CONFIGURATION = """[resource/localhost]
+enabled = yes
+type = shellcmd
+frontend = localhost
+transport = local
+max_cores_per_job = 1
+max_memory_per_core = 1GiB
+max_walltime = 8 hours
+# this doubles as "maximum concurrent jobs"
+max_cores = 4
+architecture = x86_64
+auth = none
+override = no
+"""
 
 def mock(*args, **kwargs):
     """Function used for mocking several functions imported in parallelbuild module."""
@@ -69,7 +85,7 @@ class ParallelBuildTest(EnhancedTestCase):
     """ Testcase for run module """
 
     def test_build_easyconfigs_in_parallel_pbs_python(self):
-        """Basic test for build_easyconfigs_in_parallel function."""
+        """Test build_easyconfigs_in_parallel(), using (mocked) pbs_python as backend for --job."""
         # put mocked functions in place
         PbsPython__init__ = PbsPython.__init__
         PbsPython_commit = PbsPython.commit
@@ -90,9 +106,8 @@ class ParallelBuildTest(EnhancedTestCase):
         }
         init_config(args=['--job-backend=PbsPython'], build_options=build_options)
 
-
-        easyconfig_file = os.path.join(os.path.dirname(__file__), 'easyconfigs', 'gzip-1.5-goolf-1.4.10.eb')
-        easyconfigs = process_easyconfig(easyconfig_file)
+        ec_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'easyconfigs', 'gzip-1.5-goolf-1.4.10.eb')
+        easyconfigs = process_easyconfig(ec_file)
         ordered_ecs = resolve_dependencies(easyconfigs)
         jobs = build_easyconfigs_in_parallel("echo %(spec)s", ordered_ecs, prepare_first=False)
         self.assertEqual(len(jobs), 8)
@@ -103,6 +118,39 @@ class ParallelBuildTest(EnhancedTestCase):
         PbsPython.connect_to_server = PbsPython_connect_to_server
         PbsPython.ppn = PbsPython_ppn
         pbs_python.PbsJob = pbs_python_PbsJob
+
+    def test_build_easyconfigs_in_parallel_gc3pie(self):
+        """Test build_easyconfigs_in_parallel(), using GC3Pie with local config as backend for --job."""
+        try:
+            import gc3libs
+        except ImportError:
+            print "GC3Pie not available, skipping test"
+            return
+
+        build_options = {
+            'robot_path': os.path.join(os.path.dirname(__file__), 'easyconfigs'),
+            'silent': True,
+            'valid_module_classes': config.module_classes(),
+            'validate': False,
+        }
+        init_config(args=['--job-backend=GC3Pie'], build_options=build_options)
+
+        # put GC3Pie config in place to use local host and fork/exec
+        gc3pie_cfgfile = os.path.join(self.test_prefix, 'gc3pie_local.ini')
+        write_file(gc3pie_cfgfile, GC3PIE_LOCAL_CONFIGURATION)
+        # inject configuration file
+        gc3libs.Default.CONFIG_FILE_LOCATIONS.append(gc3pie_cfgfile)
+
+        ec_file = os.path.join(os.path.dirname(__file__), 'easyconfigs', 'toy-0.0.eb')
+        easyconfigs = process_easyconfig(ec_file)
+        ordered_ecs = resolve_dependencies(easyconfigs)
+        topdir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+        test_easyblocks_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'sandbox')
+        cmd = "PYTHONPATH=%s:%s:$PYTHONPATH eb %%(spec)s -df" % (topdir, test_easyblocks_path)
+        jobs = build_easyconfigs_in_parallel(cmd, ordered_ecs, prepare_first=False)
+
+        self.assertTrue(os.path.join(self.test_installpath, 'modules', 'all', 'toy', '0.0'))
+        self.assertTrue(os.path.join(self.test_installpath, 'software', 'toy', '0.0', 'bin', 'toy'))
 
 def suite():
     """ returns all the testcases in this module """
