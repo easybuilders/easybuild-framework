ae88385de4bf3ba00926589a3229c847ca83ecd6
Kenneth Hoste <kenneth.hoste@ugent.be>
2015-06-15 12:56:29 +0200

    stop making ModulesTool class a singleton

--- test/framework/modulestool.py
+++ test/framework/modulestool.py
@@ -101,7 +101,6 @@ class ModulesToolTest(EnhancedTestCase):
 
         os.environ[BrokenMockModulesTool.COMMAND_ENVIRONMENT] = MockModulesTool.COMMAND
         os.environ['module'] = "() { /bin/echo $*\n}"
-        BrokenMockModulesTool._instances.pop(BrokenMockModulesTool, None)
         bmmt = BrokenMockModulesTool(mod_paths=[], testing=True)
         cmd_abspath = which(MockModulesTool.COMMAND)
 
@@ -135,13 +134,11 @@ class ModulesToolTest(EnhancedTestCase):
 
         # redefine 'module' function with correct module command
         os.environ['module'] = "() {  eval `/bin/echo $*`\n}"
-        MockModulesTool._instances.pop(MockModulesTool)
         mt = MockModulesTool(testing=True)
         self.assertTrue(isinstance(mt.loaded_modules(), list))  # dummy usage
 
         # a warning should be logged if the 'module' function is undefined
         del os.environ['module']
-        MockModulesTool._instances.pop(MockModulesTool)
         mt = MockModulesTool(testing=True)
         f = open(self.logfile, 'r')
         logtxt = f.read()
