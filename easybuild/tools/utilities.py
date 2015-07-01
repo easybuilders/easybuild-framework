1482a6fdb41944a66218a04bb48633d1188aee92
Caroline De Brouwer <debrouwercaroline@gmail.com>
2015-07-01 14:59:02 +0200

    Apparently im bad with whitespace

--- easybuild/tools/utilities.py
+++ easybuild/tools/utilities.py
@@ -61,7 +61,7 @@ def quote_str(x, escape_newline=False):
     Obtain a new value to be used in string replacement context.
 
     For non-string values, it just returns the exact same value.
-    
+ 
     For string values, it tries to escape the string in quotes, e.g.,
     foo becomes 'foo', foo'bar becomes "foo'bar",
     foo'bar"baz becomes \"\"\"foo'bar"baz\"\"\", etc.
