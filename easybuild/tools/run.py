f9969a834b424f05d2f40d59f795bfdd393cf1e3
molden <martijn.oldenhof@kuleuven.be>
2015-10-07 16:46:13 +0200

    give easyblocks the possibility to choose maxhits for run_cmd_qa

--- easybuild/tools/run.py
+++ easybuild/tools/run.py
@@ -130,7 +130,7 @@ def run_cmd(cmd, log_ok=True, log_all=False, simple=False, inp=None, regexp=True
     return parse_cmd_output(cmd, stdouterr, ec, simple, log_all, log_ok, regexp)
 
 
-def run_cmd_qa(cmd, qa, no_qa=None, log_ok=True, log_all=False, simple=False, regexp=True, std_qa=None, path=None):
+def run_cmd_qa(cmd, qa, no_qa=None, log_ok=True, log_all=False, simple=False, regexp=True, std_qa=None, path=None, maxhits=50):
     """
     Executes a command cmd
     - looks for questions and tries to answer based on qa dictionary
@@ -227,7 +227,7 @@ def run_cmd_qa(cmd, qa, no_qa=None, log_ok=True, log_all=False, simple=False, re
     else:
         runLog = None
 
-    maxHitCount = 50
+    maxHitCount = maxhits 
 
     try:
         p = Popen(cmd, shell=True, stdout=PIPE, stderr=STDOUT, stdin=PIPE, close_fds=True, executable="/bin/bash")
