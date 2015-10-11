febf4fee1eb8ff4fcb25b05d856d7c6dbb490197
Kenneth Hoste <kenneth.hoste@ugent.be>
2015-10-11 09:37:16 +0200

    fix typo

--- easybuild/toolchains/mpi/mpich.py
+++ easybuild/toolchains/mpi/mpich.py
@@ -52,7 +52,7 @@ class Mpich(Mpi):
     #MPI_COMPILER_MPIFC = 'mpifort'
 
     # clear MPI wrapper command options
-    MPI_SHARED_OPTION_MAP = dict([('_opt_%s' % var, '') for var in MPI_COMPILER_VARIABLES])
+    MPI_SHARED_OPTION_MAP = dict([('_opt_%s' % var[0], '') for var in MPI_COMPILER_VARIABLES])
 
     def _set_mpi_compiler_variables(self):
         """Set the MPICH_{CC, CXX, F77, F90, FC} variables."""
