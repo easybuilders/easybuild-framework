9f120e1c109b085da020a945101da6cdd5af36cd
Alan <a.ocais@fz-juelich.de>
2016-01-14 10:01:54 +0100

    Allow fixed install dir scheme

--- easybuild/tools/module_naming_scheme/mns.py
+++ easybuild/tools/module_naming_scheme/mns.py
@@ -29,10 +29,14 @@ Module naming scheme API.
 @author: Kenneth Hoste (Ghent University)
 """
 import re
+import os
+
 from vsc.utils import fancylogger
 from vsc.utils.patterns import Singleton
 
 from easybuild.tools.build_log import EasyBuildError
+from easybuild.tools.config import build_option
+from easybuild.tools.module_naming_scheme.utilities import det_full_ec_version
 
 
 class ModuleNamingScheme(object):
@@ -94,7 +98,10 @@ class ModuleNamingScheme(object):
         @return: string with name of subdirectory, e.g.: '<compiler>/<mpi_lib>/<name>/<version>'
         """
         # by default: use full module name as name for install subdir
-        return self.det_full_module_name(ec)
+        if build_option('fixed_installdir_naming_scheme'):
+            return os.path.join(ec['name'], det_full_ec_version(ec))
+        else:
+            return self.det_full_module_name(ec)
 
     def det_module_subdir(self, ec):
         """
