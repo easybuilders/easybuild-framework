239d9263ca4ce08245bf01db990dbbf81bc79791
Ward Poelmans <wpoely86@gmail.com>
2016-01-21 15:38:48 +0100

    Added wrappers for shutil.copytree and os.chdir
    To be used for the extended dry run.

--- easybuild/tools/filetools.py
+++ easybuild/tools/filetools.py
@@ -1003,80 +1003,9 @@ def cleanup(logfile, tempdir, testing, silent=False):
 
 
 def copytree(src, dst, symlinks=False, ignore=None):
-    """
-    Copied from Lib/shutil.py in python 2.7, since we need this to work for python2.4 aswell
-    and this code can be improved...
-
-    Recursively copy a directory tree using copy2().
-
-    The destination directory must not already exist.
-    If exception(s) occur, an Error is raised with a list of reasons.
-
-    If the optional symlinks flag is true, symbolic links in the
-    source tree result in symbolic links in the destination tree; if
-    it is false, the contents of the files pointed to by symbolic
-    links are copied.
-
-    The optional ignore argument is a callable. If given, it
-    is called with the `src` parameter, which is the directory
-    being visited by copytree(), and `names` which is the list of
-    `src` contents, as returned by os.listdir():
-
-        callable(src, names) -> ignored_names
-
-    Since copytree() is called recursively, the callable will be
-    called once for each directory that is copied. It returns a
-    list of names relative to the `src` directory that should
-    not be copied.
-
-    XXX Consider this example code rather than the ultimate tool.
-
-    """
-    class Error(EnvironmentError):
-        pass
-    try:
-        WindowsError  # @UndefinedVariable
-    except NameError:
-        WindowsError = None
-
-    names = os.listdir(src)
-    if ignore is not None:
-        ignored_names = ignore(src, names)
-    else:
-        ignored_names = set()
-    _log.debug("copytree: skipping copy of %s" % ignored_names)
-    os.makedirs(dst)
-    errors = []
-    for name in names:
-        if name in ignored_names:
-            continue
-        srcname = os.path.join(src, name)
-        dstname = os.path.join(dst, name)
-        try:
-            if symlinks and os.path.islink(srcname):
-                linkto = os.readlink(srcname)
-                os.symlink(linkto, dstname)
-            elif os.path.isdir(srcname):
-                copytree(srcname, dstname, symlinks, ignore)
-            else:
-                # Will raise a SpecialFileError for unsupported file types
-                shutil.copy2(srcname, dstname)
-        # catch the Error from the recursive copytree so that we can
-        # continue with other files
-        except Error, err:
-            errors.extend(err.args[0])
-        except EnvironmentError, why:
-            errors.append((srcname, dstname, str(why)))
-    try:
-        shutil.copystat(src, dst)
-    except OSError, why:
-        if WindowsError is not None and isinstance(why, WindowsError):
-            # Copying file access times may fail on Windows
-            pass
-        else:
-            errors.extend((src, dst, str(why)))
-    if errors:
-        raise Error(errors)
+    """Has been replaced by copy_files"""
+    _log.deprecated("easybuild.tools.filetools.copytree has been replaced by copy_files", '3.0')
+    copy_files(src, dst, symlinks=symlinks, ignore=ignore)
 
 
 def encode_string(name):
@@ -1158,3 +1087,38 @@ def det_size(path):
         _log.warn("Could not determine install size: %s" % err)
 
     return installsize
+
+
+def copy_files(src, dst, symlinks=False, ignore=None):
+    """
+    Wrapper around shutil.copytree to nicely handle extended dry run
+    Arguments are the same as for the original function
+    The destination directoy must not exist
+    """
+    if build_option('extended_dry_run'):
+        dry_run_msg("Copying directory %s to %s" % (src, dst), silent=build_option('silent'))
+        return
+
+    if os.path.exists(dst):
+        raise EasyBuildError("Destination directory %s of copy already exists.", dst)
+
+    try:
+        _log.debug("Copying directory %s to %s" % (src, dst))
+        shutil.copytree(src, dst, symlinks=symlinks, ignore=ignore)
+    except OSError, err:
+        raise EasyBuildError("Failed to copy %s to %s: %s", src, dst, err)
+
+
+def change_to(dst):
+    """
+    Wrapper for os.chdir: change the directory to dst
+    @dst the destination directory
+    """
+    if build_option('extended_dry_run'):
+        dry_run_msg("Changing directory to %s" % dst, silent=build_option('silent'))
+        return
+
+    try:
+        os.chdir(dst)
+    except OSError, err:
+        raise EasyBuildError("Failed to move to directory %s: %s", dst, err)
