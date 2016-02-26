613fdcfd19c8df73b8fc17370e757def17f1adbf
Ward Poelmans <wpoely86@gmail.com>
2016-02-26 12:57:42 +0100

    Fix remarks

--- easybuild/framework/easyconfig/style.py
+++ easybuild/framework/easyconfig/style.py
@@ -1,11 +1,11 @@
 ##
-# Copyright 2016 Ghent University
+# Copyright 2016-2016 Ghent University
 #
 # This file is part of EasyBuild,
 # originally created by the HPC team of Ghent University (http://ugent.be/hpc/en),
 # with support of Ghent University (http://ugent.be/hpc),
 # the Flemish Supercomputer Centre (VSC) (https://vscentrum.be/nl/en),
-# the Hercules foundation (http://www.herculesstichting.be/in_English)
+# Flemish Research Foundation (FWO) (http://www.fwo.be/en)
 # and the Department of Economy, Science and Innovation (EWI) (http://www.ewi-vlaanderen.be/en).
 #
 # http://github.com/hpcugent/easybuild
@@ -43,15 +43,21 @@ _log = fancylogger.getLogger('easyconfig.style', fname=False)
 
 # any function starting with _eb_check_ will be added to the tests
 # if the test number is added to the select list. The test number is
-# definied as AXXX where A is either E or W and XXX is a 3 digit number.
+# definied as WXXX and EXXX (for warnings and errors) where XXX is a
+# 3 digit number.
+#
 # It should be mentioned in the docstring as a single word.
-# Read the pep8 docs to understand the arguments of these functions
+# Read the pep8 docs to understand the arguments of these functions:
+# https://pep8.readthedocs.org or more specifically:
+# https://pep8.readthedocs.org/en/latest/developer.html#contribute
 def _eb_check_trailing_whitespace(physical_line, lines, line_number, total_lines):
     """
     W299
     Warn about trailing whitespace, expect for the description and comments.
     This differs from the standard trailing whitespace check as that
     will warn for any trailing whitespace.
+    The arguments are explained at
+    https://pep8.readthedocs.org/en/latest/developer.html#contribute
     """
     comment_re = re.compile('^\s*#')
     if comment_re.match(physical_line):
@@ -65,7 +71,7 @@ def _eb_check_trailing_whitespace(physical_line, lines, line_number, total_lines
     # we will not issue a warning
     keys_re = re.compile("^(?P<key>[a-z_]+)\s*=\s*")
 
-    for line in reversed(lines[0:line_number]):
+    for line in reversed(lines[:line_number]):
         res = keys_re.match(line)
         if res:
             if res.group("key") == "description":
@@ -77,10 +83,10 @@ def _eb_check_trailing_whitespace(physical_line, lines, line_number, total_lines
 
 
 @only_if_module_is_available('pep8')
-def style_conformance(lst_easyconfigs, verbose=False):
+def style_conformance(easyconfigs, verbose=False):
     """
     Check the given list of easyconfigs for style
-    @param lst_easyconfigs list of file paths to easyconfigs
+    @param easyconfigs list of file paths to easyconfigs
     @param verbose print our statistics and be verbose about the errors and warning
     @return the number of warnings and errors
     """
@@ -96,16 +102,19 @@ def style_conformance(lst_easyconfigs, verbose=False):
 
     pep8style = pep8.StyleGuide(quiet=False, config_file=None)
     options = pep8style.options
+    # we deviate from standard pep8 and allow 120 chars
+    # on a line: the default of 79 is too narrow.
     options.max_line_length = 120
     # we ignore some tests
     # note that W291 has be replaced by our custom W299
-    options.ignore = ('E402',  # import not on top
-                      'W291',  # replaced by W299
-                      'E501',  # line too long
-                      )
+    options.ignore = (
+        'E402',  # import not on top
+        'W291',  # replaced by W299
+        'E501',  # line too long
+    )
     options.verbose = int(verbose)
 
-    result = pep8style.check_files(lst_easyconfigs)
+    result = pep8style.check_files(easyconfigs)
 
     if verbose:
         result.print_statistics()
