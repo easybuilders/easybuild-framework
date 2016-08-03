772d3447142433a9a3e3dd89daa5f1581975ce72
Caylo <debrouwercaroline@gmail.com>
2016-08-03 13:47:06 +0200

    fix remarks

--- easybuild/tools/github.py
+++ easybuild/tools/github.py
@@ -863,7 +863,7 @@ def check_github():
             git_check &= attr in dir(git)
 
         if git_check:
-            check_res = "OK (gitpython version %s)" % sys.modules['git'].__version__
+            check_res = "OK (gitpython version %s)" % git.__version__
         else:
             check_res = "FAIL (import ok, but module doesn't provide what is expected)"
     else:
@@ -896,7 +896,7 @@ def check_github():
         else:
             check_res = "OK"
     elif github_user:
-        if sys.modules['git'].__version__ < "1.0.0":
+        if LooseVersion(git.__version__) < LooseVersion('1.0'):
             check_res = "FAIL (outdated gitpython module, try updating first)"
         else:
             check_res = "FAIL (unexpected exception: %s)" % push_err
