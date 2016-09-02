e945227d0e0cf0b5abe1662e7d4d8e6812fc596c
Kenneth Hoste <kenneth.hoste@ugent.be>
2016-09-02 20:42:46 +0200

    fix function signature of log.deprecated compared to fancylogger.deprecated

--- easybuild/tools/build_log.py
+++ easybuild/tools/build_log.py
@@ -120,10 +120,20 @@ class EasyBuildLog(fancylogger.FancyLogger):
             msg = 'Experimental functionality. Behaviour might change/be removed later (use --experimental option to enable). ' + msg
             raise EasyBuildError(msg, *args)
 
-    def deprecated(self, msg, max_ver):
-        """Print deprecation warning or raise an EasyBuildError, depending on max version allowed."""
-        msg += "; see %s for more information" % DEPRECATED_DOC_URL
-        fancylogger.FancyLogger.deprecated(self, msg, str(CURRENT_VERSION), max_ver, exception=EasyBuildError)
+    def deprecated(self, msg, ver, max_ver=None):
+        """
+        Print deprecation warning or raise an exception, depending on specified version(s)
+
+        :param: msg: deprecation message
+        :param ver: if max_ver is None: threshold for EasyBuild version to determine warning vs exception
+                    else: version to check against max_ver to determine warning vs exception
+        :param max_ver: version threshold for warning vs exception (compared to 'ver')
+        """
+        if max_ver is None:
+            msg += "; see %s for more information" % DEPRECATED_DOC_URL
+            fancylogger.FancyLogger.deprecated(self, msg, str(CURRENT_VERSION), max_ver, exception=EasyBuildError)
+        else:
+            fancylogger.FancyLogger.deprecated(self, msg, ver, max_ver)
 
     def nosupport(self, msg, ver):
         """Print error message for no longer supported behaviour, and raise an EasyBuildError."""
