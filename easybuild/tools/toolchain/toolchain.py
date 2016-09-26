895c19a6ed8cd4225e35edc8bb61446c3ea13dd8
Kenneth Hoste <kenneth.hoste@ugent.be>
2016-09-26 21:30:41 +0200

    implement embedded fallthrough RPATH ld wrapper script

--- easybuild/tools/toolchain/toolchain.py
+++ easybuild/tools/toolchain/toolchain.py
@@ -32,17 +32,17 @@ Creating a new toolchain should be as simple as possible.
 """
 import copy
 import os
+import stat
 import tempfile
 from vsc.utils import fancylogger
 
 from easybuild.tools.build_log import EasyBuildError, dry_run_msg
 from easybuild.tools.config import build_option, install_path
 from easybuild.tools.environment import setvar
-from easybuild.tools.filetools import which
+from easybuild.tools.filetools import adjust_permissions, which, write_file
 from easybuild.tools.module_generator import dependencies_for
 from easybuild.tools.modules import get_software_root, get_software_root_env_var_name
 from easybuild.tools.modules import get_software_version, get_software_version_env_var_name
-from easybuild.tools.rpath import prepare_ld_wrapper
 from easybuild.tools.toolchain import DUMMY_TOOLCHAIN_NAME, DUMMY_TOOLCHAIN_VERSION
 from easybuild.tools.toolchain.options import ToolchainOptions
 from easybuild.tools.toolchain.toolchainvariables import ToolchainVariables
@@ -52,6 +52,40 @@ _log = fancylogger.getLogger('tools.toolchain', fname=False)
 
 CCACHE = 'ccache'
 F90CACHE = 'f90cache'
+RPATH_LD_WRAPPER = """#!/bin/bash
+
+DEBUG=${RPATH_LD_WRAPPER_DEBUG:false}
+CMD=$(basename $0)
+ALL_CMDS=( $( type -p -a $CMD ) )
+
+function ERROR {
+    echo "ERROR (RPATH_LD_WRAPPER): $1"
+    exit 1
+}
+
+# we should at least find two instances of $CMD
+if [ ${#ALL_CMDS[*]} -ge 2 ]; then
+
+    ORIG_CMD=${ALL_CMDS[1]}
+
+    # 1st instance of $CMD must be this script
+    if [ ${ALL_CMDS[0]} != $0 ]; then
+        ERROR "Expected to find 1st instance of '$CMD' to be $0"
+
+    # 2nd instance of command must be the original $CMD (i.e., not this script)
+    elif [ $ORIG_CMD == $0 ]; then
+        ERROR "Did not expect to find 2nd instance of '$CMD' to be $0"
+    fi
+else
+    ERROR "Failed to find original '$CMD'; 'which -a $CMD' => ${ALL_CMDS[*]}"
+fi
+
+$DEBUG && echo "RPATH_LD_WRAPPER: found CMD: $CMD, ALL_CMDS: ${ALL_CMDS[*]}, ORIG_CMD: $ORIG_CMD"
+
+$DEBUG && echo ">> $ORIG_CMD $@"
+$ORIG_CMD $@
+
+"""
 
 
 class Toolchain(object):
@@ -672,7 +706,7 @@ class Toolchain(object):
                 self.prepare_compiler_cache(cache_tool)
 
         if build_option('rpath'):
-            prepare_rpath_wrapper()
+            self.prepare_rpath_ld_wrapper()
 
     def comp_cache_compilers(self, cache_tool):
         """
@@ -722,20 +756,26 @@ class Toolchain(object):
         self.cached_compilers.update(compilers)
         self.log.debug("Cached compilers (after preparing for %s): %s", cache_tool, self.cached_compilers)
 
-    def prepare_rpath_wrapper(self):
+    def prepare_rpath_ld_wrapper(self):
         """
         Put RPATH wrapper script in place for compiler and linker commands
         """
         # FIXME RPATH is linux only
-        compiler_cmds = [comp for comps in self.compilers() for comp in comps]
-        linker_cmds = ['ld', 'ld.gold']
 
-        # FIXME figure out location of RPATH wrapper (or include as a constant?)
-        # FIXME ensure execution permissions
-        # FIXME enable debug module for RPATH wrapper based --debug
-        rpath_wrapper = None
+        rpath_ld_wrapper_path = os.path.join(tempfile.gettempdir(), 'rpath_ld_wrapper.sh')
+        write_file(rpath_ld_wrapper_path, RPATH_LD_WRAPPER)
+        adjust_permissions(rpath_ld_wrapper_path, stat.S_IXUSR)
 
-        self.symlink_commands({'rpath': (rpath_wrapper, compiler_cmds + linker_cmds)})
+        # FIXME debug messages always appear?!
+        if build_option('debug'):
+            os.environ['RPATH_LD_WRAPPER_DEBUG'] = 'true'
+
+        c_comps, fortran_comps = self.compilers()
+        # FIXME: alternative to symlinking script: generate dedicated version of script for each command,
+        # hardcode path to original command in the script rather than hunting for it...
+        # => less runtime overhead & safer...
+        # FIXME: must also wrap compilers commands, required e.g. for Clang ('gcc' on OS X)?
+        self.symlink_commands({'rpath': (rpath_ld_wrapper_path, c_comps + fortran_comps + ['ld', 'ld.gold'])})
 
     def _add_dependency_variables(self, names=None, cpp=None, ld=None):
         """ Add LDFLAGS and CPPFLAGS to the self.variables based on the dependencies
