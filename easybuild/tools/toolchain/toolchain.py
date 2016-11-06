5e992d5a5d06172a7521a47f81194c757c111d98
Kenneth Hoste <kenneth.hoste@ugent.be>
2016-11-06 21:57:47 +0100

    determine path to easybuild/scripts via easybuild.tools module

--- easybuild/tools/toolchain/toolchain.py
+++ easybuild/tools/toolchain/toolchain.py
@@ -38,6 +38,7 @@ import tempfile
 from vsc.utils import fancylogger
 from vsc.utils.missing import nub
 
+import easybuild.tools
 from easybuild.tools.build_log import EasyBuildError, dry_run_msg
 from easybuild.tools.config import build_option, install_path
 from easybuild.tools.environment import setvar
@@ -740,18 +741,18 @@ class Toolchain(object):
         c_comps, fortran_comps = self.compilers()
 
         # determine path to Python script that interprets/filters command arguments and defines $RPATH
-        eb_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+        eb_dir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(easybuild.tools.__file__))))
         rpath_args_py = os.path.join(eb_dir, 'scripts', 'rpath_args.py')
         if os.path.exists(rpath_args_py):
             self.log.info("Python script for RPATH wrapper found: %s", rpath_args_py)
         else:
-            raise EasyBuildError("Failed to find Python script for RPATH wrapper: %s (__file__: %s)", rpath_args_py, os.path.realpath(__file__))
+            raise EasyBuildError("Failed to find Python script for RPATH wrapper: %s", rpath_args_py)
 
         rpath_wrapper_template = os.path.join(eb_dir, 'scripts', 'rpath_wrapper_template.sh')
         if os.path.exists(rpath_wrapper_template):
             self.log.info("RPATH wrapper template script found: %s", rpath_wrapper_template)
         else:
-            raise EasyBuildError("Failed to find RPATH wrapper script: %s (__file__: %s)", rpath_wrapper_template, os.path.realpath(__file__))
+            raise EasyBuildError("Failed to find RPATH wrapper script: %s", rpath_wrapper_template)
 
         # prepend location to wrappers to $PATH
         setvar('PATH', '%s:%s' % (wrapper_dir, os.getenv('PATH')))
