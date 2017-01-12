79e874768387c889d8793f4fdb8a1d6f3e8b04ec
Bart Oldeman <bart.oldeman@calculquebec.ca>
2017-01-11 21:58:57 -0500

    Allow linalg without MPI (skip BLACS/SCALAPACK if usempi not set)

--- easybuild/tools/toolchain/linalg.py
+++ easybuild/tools/toolchain/linalg.py
@@ -90,8 +90,9 @@ class LinAlg(Toolchain):
         ## TODO is link order fully preserved with this order ?
         self._set_blas_variables()
         self._set_lapack_variables()
-        self._set_blacs_variables()
-        self._set_scalapack_variables()
+        if self.options.get('usempi', False):
+            self._set_blacs_variables()
+            self._set_scalapack_variables()
 
         self.log.debug('set_variables: LinAlg variables %s' % self.variables)
 
