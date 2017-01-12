d85848177787a7c691810a2a1fa1a4545a0e1a88
Bart Oldeman <bart.oldeman@calculquebec.ca>
2017-01-12 14:41:50 -0500

    Do not rely on specific order in fftw_libs as suggested by @boegel

--- easybuild/toolchains/fft/intelfftw.py
+++ easybuild/toolchains/fft/intelfftw.py
@@ -66,13 +66,17 @@ class IntelFFTW(Fftw):
             else:
                 raise EasyBuildError("Not using Intel compilers, PGI nor GCC, don't know compiler suffix for FFTW libraries.")
 
-        fftw_libs = ["fftw3xc%s%s" % (compsuff, picsuff)]
+        interface_lib = "fftw3xc%s%s" % (compsuff, picsuff)
+        fftw_libs = [interface_lib]
         if self.options.get('usempi', False):
             # add cluster interface for recent imkl versions
+            cluster_interface_lib = None
             if LooseVersion(imklver) >= LooseVersion("11.0.2"):
-                fftw_libs.append("fftw3x_cdft%s%s" % (bitsuff, picsuff))
+                cluster_interface_lib = "fftw3x_cdft%s%s" % (bitsuff, picsuff)
+                fftw_libs.append(cluster_interface_lib)
             elif LooseVersion(imklver) >= LooseVersion("10.3"):
-                fftw_libs.append("fftw3x_cdft%s" % picsuff)
+                cluster_interface_lib = "fftw3x_cdft%s" % picsuff
+                fftw_libs.append(cluster_interface_lib)
             fftw_libs.append("mkl_cdft_core")  # add cluster dft
             fftw_libs.extend(self.variables['LIBBLACS'].flatten()) # add BLACS; use flatten because ListOfList
 
@@ -90,13 +94,13 @@ class IntelFFTW(Fftw):
         # filter out libraries from list of FFTW libraries to check for if they are not provided by Intel MKL
         check_fftw_libs = [lib for lib in fftw_libs if lib not in ['dl', 'gfortran']]
         fftw_lib_exists = lambda x: any([os.path.exists(os.path.join(d, "lib%s.a" % x)) for d in fft_lib_dirs])
-        if not fftw_lib_exists(check_fftw_libs[0]) and LooseVersion(imklver) >= LooseVersion("10.2"):
+        if not fftw_lib_exists(interface_lib) and LooseVersion(imklver) >= LooseVersion("10.2"):
+            # interface libs can be optional:
             # MKL >= 10.2 include fftw3xc and fftw3xf interfaces in LIBBLAS=libmkl_gf/libmkl_intel
             # See https://software.intel.com/en-us/articles/intel-mkl-main-libraries-contain-fftw3-interfaces
-            check_fftw_libs.pop(0)
-            # and reduce functionality with MPI if wrappers are not built
-            if check_fftw_libs[0].startswith("fftw3x_cdft"):
-                check_fftw_libs.pop(0)
+            # The cluster interface libs (libfftw3x_cdft*) can be omitted if the toolchain does not provide MPI-FFTW
+            # interfaces.
+            check_fftw_libs = [lib for lib in check_fftw_libs if lib != interface_lib and lib != cluster_interface_lib]
         if all([fftw_lib_exists(lib) for lib in check_fftw_libs]):
             self.FFT_LIB = fftw_libs
         else:
