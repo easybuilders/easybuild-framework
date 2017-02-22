7f4b1507c773bacb994a84193c4e5688ea9def2f
Maxime Boissonneault <maxime.boissonneault@calculquebec.ca>
2017-02-22 17:00:48 +0000

    modified the extraction to use the built-in extract_file

--- easybuild/tools/filetools.py
+++ easybuild/tools/filetools.py
@@ -766,7 +766,7 @@ def apply_patch(patch_file, dest, fn=None, copy=False, level=None):
     if apatch.endswith(".gz"):
         _log.debug("Ungzipping the patch")
         # gunzipping the patch. Force overwriting if a previous version was already gunzipped.
-        run.run_cmd("gunzip --force --keep %s" % apatch)
+        extract_file(apatch,os.path.dirname(apatch),overwrite=True)
         # remove the '.gz' extension
         apatch = apatch[:-3]
 
