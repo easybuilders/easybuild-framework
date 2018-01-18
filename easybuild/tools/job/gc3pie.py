81dd24cf87a125f9359980ab63638810146ac720
Riccardo Murri <riccardo.murri@gmail.com>
2018-01-18 16:40:30 +0100

    The correct parameter for capping nr. of concurrent jobs is `gc3libs.Engine.max_in_flight`
    Many thanks to Theofilos Manitaras for noticing and suggesting the fix!

--- easybuild/tools/job/gc3pie.py
+++ easybuild/tools/job/gc3pie.py
@@ -220,7 +220,7 @@ class GC3Pie(JobBackend):
         self._engine.retrieve_overwrites = True
 
         # some sites may not be happy with flooding the cluster with build jobs...
-        self._engine.limit_in_flight = build_option('job_max_jobs')
+        self._engine.max_in_flight = build_option('job_max_jobs')
 
         # `Engine.stats()` (which is used later on in `_print_status_report()`)
         # changed between 2.4.2 and 2.5.0.dev -- make sure we stay compatible
