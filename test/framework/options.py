14f8a7ccd1069832d7dc4d7403d2fa448a332e7b
Kenneth Hoste <kenneth.hoste@ugent.be>
2018-08-20 19:58:31 +0200

    rework test_check_style into test_check_contrib_style to also cover style aspects of --check-contrib

--- test/framework/options.py
+++ test/framework/options.py
@@ -3200,23 +3200,27 @@ class CommandLineOptionsTest(EnhancedTestCase):
             options.postprocess()
             self.assertEqual(options.options.optarch, optarch_parsed)
 
-    def test_check_style(self):
-        """Test --check-style."""
+    def test_check_contrib_style(self):
+        """Test style checks performed by --check-contrib + dedicated --check-style option."""
         try:
-            import pep8
+            import pycodestyle
         except ImportError:
-            print "Skipping test_check_style, since pep8 is not available"
-            return
+            try:
+                import pep8
+            except ImportError:
+                print "Skipping test_check_contrib_style, since pycodestyle or pep8 is not available"
+                return
 
-        args = [
-            '--check-style',
-            'GCC-4.9.2.eb',
-            'toy-0.0.eb',
-        ]
-        stdout, _ = self._run_mock_eb(args, raise_error=True)
+        for check_type in ['contrib', 'style']:
+            args = [
+                '--check-%s' % check_type,
+                'GCC-4.9.2.eb',
+                'toy-0.0.eb',
+            ]
+            stdout, _ = self._run_mock_eb(args, raise_error=True)
 
-        regex = re.compile(r"Running style check on 2 easyconfig\(s\)", re.M)
-        self.assertTrue(regex.search(stdout), "Pattern '%s' found in: %s" % (regex.pattern, stdout))
+            regex = re.compile(r"Running style check on 2 easyconfig\(s\)", re.M)
+            self.assertTrue(regex.search(stdout), "Pattern '%s' found in: %s" % (regex.pattern, stdout))
 
         # copy toy-0.0.eb test easyconfig, fiddle with it to make style check fail
         toy = os.path.join(self.test_prefix, 'toy.eb')
@@ -3229,22 +3233,24 @@ class CommandLineOptionsTest(EnhancedTestCase):
         toytxt = toytxt.replace('description = "Toy C program, 100% toy."', 'description = "%s"' % ('toy ' * 30))
         write_file(toy, toytxt)
 
-        args = [
-            '--check-style',
-            toy,
-        ]
-        self.mock_stdout(True)
-        self.assertErrorRegex(EasyBuildError, "One or more style checks FAILED!", self.eb_main, args, raise_error=True)
-        stdout = self.get_stdout()
-        self.mock_stdout(False)
-        patterns = [
-            "toy.eb:1:5: E223 tab before operator",
-            "toy.eb:1:7: E225 missing whitespace around operator",
-            "toy.eb:1:12: W299 trailing whitespace",
-            r"toy.eb:5:121: E501 line too long \(136 > 120 characters\)",
-        ]
-        for pattern in patterns:
-            self.assertTrue(re.search(pattern, stdout, re.M), "Pattern '%s' found in: %s" % (pattern, stdout))
+        for check_type in ['contribution', 'style']:
+            args = [
+                '--check-%s' % check_type[:7],
+                toy,
+            ]
+            self.mock_stdout(True)
+            error_pattern = "One or more %s checks FAILED!" % check_type
+            self.assertErrorRegex(EasyBuildError, error_pattern, self.eb_main, args, raise_error=True)
+            stdout = self.get_stdout()
+            self.mock_stdout(False)
+            patterns = [
+                "toy.eb:1:5: E223 tab before operator",
+                "toy.eb:1:7: E225 missing whitespace around operator",
+                "toy.eb:1:12: W299 trailing whitespace",
+                r"toy.eb:5:121: E501 line too long \(136 > 120 characters\)",
+            ]
+            for pattern in patterns:
+                self.assertTrue(re.search(pattern, stdout, re.M), "Pattern '%s' found in: %s" % (pattern, stdout))
 
     def test_allow_use_as_root(self):
         """Test --allow-use-as-root-and-accept-consequences"""
