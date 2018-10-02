9ee91f9a18e3a20416d5f4b385de0bffb336bdbd
Bart Oldeman <bart.oldeman@calculquebec.ca>
2018-10-02 13:24:04 +0000

    @houndci-bot

--- easybuild/framework/easyconfig/tweak.py
+++ easybuild/framework/easyconfig/tweak.py
@@ -125,7 +125,8 @@ def tweak(easyconfigs, build_specs, modtool, targetdirs=None):
                     raise EasyBuildError("Toolchain %s is not equivalent to toolchain %s in terms of capabilities. "
                                          "Use --force if you are sure.",
                                          target_toolchain['name'], source_toolchain['name'])
-                print_warning("Combining --try-toolchain with --force for toolchains with unequal capabilities: using regex.")
+                print_warning("Combining --try-toolchain with --force for toolchains with unequal capabilities: "
+                              "using regex.")
                 revert_to_regex = True
                 modifying_toolchains = False
 
