eb23b01569cc41580f0f1f5f0bddb862aa94e44f
Maxime Boissonneault <maxime.boissonneault@calculquebec.ca>
2018-11-14 18:35:23 +0000

    fixing bug introduced in update

--- easybuild/framework/easyconfig/easyconfig.py
+++ easybuild/framework/easyconfig/easyconfig.py
@@ -447,8 +447,9 @@ class EasyConfig(object):
         Update a string configuration value with a value (i.e. append to it).
         """
         prev_value = self[key]
-        if isinstance(prev_value, basestring) and (allow_duplicate or value not in prev_value):
-            self[key] = '%s %s ' % (prev_value, value)
+        if isinstance(prev_value, basestring)
+            if allow_duplicate or value not in prev_value:
+                self[key] = '%s %s ' % (prev_value, value)
         elif isinstance(prev_value, list):
             if allow_duplicate:
                 self[key] = prev_value + value
