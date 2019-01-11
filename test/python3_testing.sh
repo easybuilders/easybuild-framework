0edc6bbdd9b1068735ad0aa73d7e9a7cf85f0bcc
Kenneth Hoste <kenneth.hoste@ugent.be>
2019-01-11 18:56:14 +0100

    fix test/python3_testing.sh script

--- test/python3_testing.sh
+++ test/python3_testing.sh
@@ -3,5 +3,5 @@ echo "testing with `which python`: `python3 -V`"
 for mod in `ls easybuild/base/*.py | grep -v __init__ | cut -f3 -d/ | sed 's/.py$//g'`; do
     test="import easybuild.base.$mod"
     echo $test
-    python3 -c $test
+    python3 -c "$test"
 done
