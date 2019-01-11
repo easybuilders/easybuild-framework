afff4d9750ac7a3d7f9d8db78e3a2ada9662b0fb
Kenneth Hoste <kenneth.hoste@ugent.be>
2019-01-11 09:14:42 +0100

    add small py2vs3 module to easybuild.tools to make porting to Python 3 easier without having to rely on six/future(s)/etc.

---
+++ easybuild/tools/py2vs3/py3.py
@@ -0,0 +1,39 @@
+#
+# Copyright 2019-2019 Ghent University
+#
+# This file is part of EasyBuild,
+# originally created by the HPC team of Ghent University (http://ugent.be/hpc/en),
+# with support of Ghent University (http://ugent.be/hpc),
+# the Flemish Supercomputer Centre (VSC) (https://www.vscentrum.be),
+# Flemish Research Foundation (FWO) (http://www.fwo.be/en)
+# and the Department of Economy, Science and Innovation (EWI) (http://www.ewi-vlaanderen.be/en).
+#
+# https://github.com/easybuilders/easybuild
+#
+# EasyBuild is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation v2.
+#
+# EasyBuild is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with EasyBuild.  If not, see <http://www.gnu.org/licenses/>.
+#
+"""
+Functionality to facilitate keeping code compatible with Python 2 & Python 3.
+
+Implementations for Python 3.
+
+:author: Kenneth Hoste (Ghent University)
+"""
+
+# string type that can be used in 'isinstance' calls
+string_type = str
+
+
+def raise_with_traceback(exception_class, message, traceback):
+    """Raise exception of specified class with given message and traceback."""
+    raise exception_class(message).with_traceback(traceback)
