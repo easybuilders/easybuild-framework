c75cce3ff0ad545310b4fcb3a0581409e32ad927
Kenneth Hoste <kenneth.hoste@ugent.be>
2019-01-11 19:56:51 +0100

    also test import of easybuild.tools modules with Python 3

--- test/python3_testing.sh
+++ test/python3_testing.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 echo "testing with `which python3`: `python3 -V`"
-for mod in `ls easybuild/base/*.py | grep -v __init__ | cut -f3 -d/ | sed 's/.py$//g'`; do
-    test="import easybuild.base.$mod"
+for mod in `ls easybuild/base/*.py easybuild/tools/*py| grep -v __init__ | cut -f2-3 -d/ | tr '/' '.' | sed 's/.py$//g'`; do
+    test="import easybuild.$mod"
     echo $test
     python3 -c "$test"
 done
+python3 -c "import easybuild.tools.asyncprocess"
+python3 -c "import easybuild.tools.config"
