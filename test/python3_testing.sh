6b135594954555273784f86937ab1a97a0d178a3
Kenneth Hoste <kenneth.hoste@ugent.be>
2019-01-12 20:39:11 +0100

    consistently use python3 in test/python3_testing.sh

--- test/python3_testing.sh
+++ test/python3_testing.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "testing with `which python`: `python3 -V`"
+echo "testing with `which python3`: `python3 -V`"
 for mod in `ls easybuild/base/*.py | grep -v __init__ | cut -f3 -d/ | sed 's/.py$//g'`; do
     test="import easybuild.base.$mod"
     echo $test
