f1f067aef614a3a038ba48d53390d91fa767461d
Kenneth Hoste <kenneth.hoste@ugent.be>
2019-01-12 22:27:14 +0100

    don't test import of easybuild.tools.ordereddict in Python 3 (not used in that context)

--- test/python3_testing.sh
+++ test/python3_testing.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 echo "testing with `which python3`: `python3 -V`"
-for mod in `ls easybuild/base/*.py easybuild/tools/*py| grep -v __init__ | cut -f2-3 -d/ | tr '/' '.' | sed 's/.py$//g'`; do
+for mod in `ls easybuild/base/*.py easybuild/tools/*py | egrep -v '__init__|ordereddict' | cut -f2-3 -d/ | tr '/' '.' | sed 's/.py$//g'`; do
     test="import easybuild.$mod"
     echo $test
     python3 -c "$test"
 done
-python3 -c "import easybuild.tools.asyncprocess"
-python3 -c "import easybuild.tools.config"
