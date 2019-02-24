775ec9770d5932465388eec2caf49a0c11f65ccb
Kenneth Hoste <kenneth.hoste@ugent.be>
2019-02-24 16:26:01 +0100

    make sure that tests for --include-* options also pass without having location of easybuild-framework in listed in $PYTHONPATH

--- test/framework/options.py
+++ test/framework/options.py
@@ -2209,6 +2209,12 @@ class CommandLineOptionsTest(EnhancedTestCase):
 
     def mk_eb_test_cmd(self, args):
         """Construct test command for 'eb' with given options."""
+
+        # make sure that location to 'easybuild.main' is included in $PYTHONPATH
+        pythonpath = os.getenv('PYTHONPATH')
+        easybuild_loc = os.path.dirname(os.path.dirname(easybuild.main.__file__))
+        os.environ['PYTHONPATH'] = ':'.join([easybuild_loc, pythonpath])
+
         return '; '.join([
             "cd %s" % self.test_prefix,
             "%s -O -m easybuild.main %s" % (sys.executable, ' '.join(args)),
