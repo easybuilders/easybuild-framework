7ad7aac8c668752df7aa880a7867ab4c03ddbf52
Kenneth Hoste <kenneth.hoste@ugent.be>
2019-06-26 19:57:30 +0200

    fix test_fix_deprecated_easyconfigs

--- test/framework/easyconfig.py
+++ test/framework/easyconfig.py
@@ -2867,13 +2867,15 @@ class EasyConfigTest(EnhancedTestCase):
 
         test_ec = os.path.join(self.test_prefix, 'test.eb')
 
+        # need to allow triggering deprecated behaviour, since that's exactly what we're fixing...
+        self.allow_deprecated_behaviour()
+
         test_ectxt = toy_ec_txt
         # inject local variables with names that need to be tweaked (or not for single-letter ones)
         regex = re.compile('^(sanity_check_paths)', re.M)
         test_ectxt = regex.sub(r'foobar = "foobar"\n\n\1', toy_ec_txt)
         regex = re.compile('^(toolchain\s*=.*)$', re.M)
         test_ectxt = regex.sub(r'\1\n\nsome_list = [x + "1" for x in ["one", "two", "three"]]', test_ectxt)
-        print test_ectxt
 
         # test fixing the use of 'dummy' toolchain to SYSTEM
         tc_regex = re.compile('^toolchain = .*', re.M)
@@ -2891,6 +2893,26 @@ class EasyConfigTest(EnhancedTestCase):
             test_ec_txt = read_file(test_ec)
             regex = re.compile("^toolchain = {.*'name': 'dummy'.*$", re.M)
             self.assertTrue(regex.search(test_ec_txt), "Pattern '%s' found in: %s" % (regex.pattern, test_ec_txt))
+
+            # mimic default behaviour where only warnings are being printed
+            init_config(build_options={'strict_local_var_naming': False, 'silent': False})
+            self.mock_stderr(True)
+            self.mock_stdout(True)
+            ec = EasyConfig(test_ec)
+            stderr, stdout = self.get_stderr(), self.get_stdout()
+            self.mock_stderr(False)
+            self.mock_stdout(False)
+
+            self.assertFalse(stdout)
+
+            warnings = [
+                "WARNING: Use of 2 unknown easyconfig parameters detected: foobar, some_list",
+                "Use of 'dummy' toolchain is deprecated, use 'system' toolchain instead",
+            ]
+            for warning in warnings:
+                self.assertTrue(warning in stderr, "Found warning '%s' in stderr output: %s" % (warning, stderr))
+
+            init_config(build_options={'strict_local_var_naming': True, 'silent': True})
 
             # easyconfig doesn't parse because of local variables with name other than 'local_*'
             error_pattern = "Use of 2 unknown easyconfig parameters detected: foobar, some_list"
@@ -2915,7 +2937,10 @@ class EasyConfigTest(EnhancedTestCase):
             # original easyconfig is backed up automatically
             test_ecs = sorted([f for f in os.listdir(self.test_prefix) if f.startswith('test.eb')])
             self.assertEqual(len(test_ecs), 2)
-            self.assertEqual(test_ec_txt, read_file(os.path.join(self.test_prefix, test_ecs[1])))
+            backup_test_ec = os.path.join(self.test_prefix, test_ecs[1])
+            self.assertEqual(test_ec_txt, read_file(backup_test_ec))
+
+            remove_file(backup_test_ec)
 
             # parsing works now, toolchain is replaced with system toolchain
             ec = EasyConfig(test_ec)
