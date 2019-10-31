26dd814a315de18413448cbfd5b2df3694796ff3
Ake Sandgren <ake.sandgren@hpc2n.umu.se>
2019-10-31 10:33:07 +0100

    Forgot to increas TEST_MODULES_COUNT when adding the iccifortcuda test module

--- test/framework/modules.py
+++ test/framework/modules.py
@@ -51,7 +51,7 @@ from easybuild.tools.run import run_cmd
 
 
 # number of modules included for testing purposes
-TEST_MODULES_COUNT = 78
+TEST_MODULES_COUNT = 79
 
 
 class ModulesTest(EnhancedTestCase):
