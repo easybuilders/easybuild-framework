2bb070a4d3e96f7e6d1aa3cfe1bb3dda5a1be890
Ward Poelmans <wpoely86@gmail.com>
2019-11-15 09:55:56 +0100

    add all extensions in unittest

--- test/framework/module_generator.py
+++ test/framework/module_generator.py
@@ -577,9 +577,9 @@ class ModuleGeneratorTest(EnhancedTestCase):
 
         patterns = []
         if self.MODULE_GENERATOR_CLASS == ModuleGeneratorLua:
-            patterns.append('^extensions("bar/0.0", "barbar/0.0")')
+            patterns.append('^extensions("bar/0.0", "barbar/0.0", "l/s", "toy/0.0")')
         elif self.MODULE_GENERATOR_CLASS == ModuleGeneratorTcl:
-            patterns.append('^extensions "bar/0.0", "barbar/0.0"')
+            patterns.append('^extensions "bar/0.0", "barbar/0.0", "l/s", "toy/0.0"')
 
         for pattern in patterns:
             self.assertTrue(re.search(pattern, desc, re.M), "Pattern '%s' found in: %s" % (pattern, desc))
