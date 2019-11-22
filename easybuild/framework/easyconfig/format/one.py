d618b0c524bd85e8a83c28d92e1b966ca92ff1a4
Kenneth Hoste <kenneth.hoste@ugent.be>
2019-11-22 07:37:18 +0100

    trivial style fix

--- easybuild/framework/easyconfig/format/one.py
+++ easybuild/framework/easyconfig/format/one.py
@@ -161,9 +161,11 @@ class FormatOneZero(EasyConfigFormatConfigObj):
                 if isinstance(param_val, dict):
                     ordered_item_keys = REFORMAT_ORDERED_ITEM_KEYS.get(param_name, sorted(param_val.keys()))
                     for item_key in ordered_item_keys:
-                        if item_key not in param_val:
+                        if item_key in param_val:
+                            item_val = param_val[item_key]
+                        else:
                             raise EasyBuildError("Missing mandatory key '%s' in %s.", item_key, param_name)
-                        item_val = param_val[item_key]
+
                         comment = self._get_item_comments(param_name, item_val).get(str(item_val), '')
                         key_pref = quote_py_str(item_key) + ': '
                         addlen = addlen + len(INDENT_4SPACES) + len(key_pref) + len(comment)
