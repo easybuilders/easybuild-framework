44b68af3816f09ab8f9b38ba06cff3eade3e233f
Ake Sandgren <ake.sandgren@hpc2n.umu.se>
2020-03-05 19:52:32 +0100

    Appease Hound

--- easybuild/toolchains/mpi/intelmpi.py
+++ easybuild/toolchains/mpi/intelmpi.py
@@ -80,9 +80,9 @@ class IntelMPI(Mpich2):
 
             for root in self.get_software_root(self.MPI_MODULE_NAME):
                 self.variables.append_exists('MPI_LIB_STATIC', root, lib_dir,
-                                            filename="lib%s.a" % self.MPI_LIBRARY_NAME)
+                                             filename="lib%s.a" % self.MPI_LIBRARY_NAME)
                 self.variables.append_exists('MPI_LIB_SHARED', root, lib_dir,
-                                            filename="lib%s.so" % self.MPI_LIBRARY_NAME)
+                                             filename="lib%s.so" % self.MPI_LIBRARY_NAME)
                 self.variables.append_exists('MPI_LIB_DIR', root, lib_dir)
                 self.variables.append_exists('MPI_INC_DIR', root, incl_dir)
 
