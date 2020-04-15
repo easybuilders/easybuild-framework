3220e99a1fe655562a55052fc4b109570e883b8c
Alexander Grund <alexander.grund@tu-dresden.de>
2020-04-15 16:46:56 +0200

    Add CPU architecture as found by EB to test report

--- easybuild/tools/systemtools.py
+++ easybuild/tools/systemtools.py
@@ -770,6 +770,7 @@ def get_system_info():
     return {
         'core_count': get_avail_core_count(),
         'total_memory': get_total_memory(),
+        'cpu_arch': get_cpu_architecture(),
         'cpu_arch_name': get_cpu_arch_name(),
         'cpu_model': get_cpu_model(),
         'cpu_speed': get_cpu_speed(),
