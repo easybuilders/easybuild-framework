b37367cdad07e54ffbca5b3cfa72b26caf5e120c
Terje Kvernes <terje@kvernes.no>
2020-05-04 15:44:03 +0200

    The assert failure message, redone. Again.
      - Based on @boegel's phrasing request, from lessons related to
        the lack of tests not being fully isolated from the
        environment. See the following issue for more information:
        https://github.com/easybuilders/easybuild-framework/issues/3231
      - Also added back the stringification of the types, including
        the tuple, by wrapping the output in str().

--- test/framework/easyconfigparser.py
+++ test/framework/easyconfigparser.py
@@ -184,8 +184,9 @@ class EasyConfigParserTest(EnhancedTestCase):
         for constant_name in constants:
             self.assertTrue(isinstance(constant_name, string_type), "Constant name %s is a string" % constant_name)
             val = constants[constant_name]
-            self.assertTrue(isinstance(val, (string_type, dict, tuple)),
-                            "The constant %s has an acceptable type" % constant_name)
+            fail_msg = "The constant %s should have an acceptable type, found %s (%s)"
+            % (constant_name, type(val), str(val))
+            self.assertTrue(isinstance(val, (string_type, dict, tuple)), fail_msg)
 
         # check a couple of randomly picked constant values
         self.assertEqual(constants['SOURCE_TAR_GZ'], '%(name)s-%(version)s.tar.gz')
