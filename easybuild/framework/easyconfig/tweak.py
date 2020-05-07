fa8c9a6855c8b52352b0538eccbb4ae7ceec8c7c
Alan O'Cais <a.ocais@fz-juelich.de>
2020-05-07 08:43:29 +0200

    Make error for version suffix mapping more explicit

--- easybuild/framework/easyconfig/tweak.py
+++ easybuild/framework/easyconfig/tweak.py
@@ -907,8 +907,9 @@ def map_common_versionsuffixes(software_name, original_toolchain, toolchain_mapp
                     if original_suffix in versionsuffix_mappings:
                         if mapped_suffix != versionsuffix_mappings[original_suffix]:
                             raise EasyBuildError("No unique versionsuffix mapping for %s in %s toolchain "
-                                                 "hierarchy to %s toolchain hierarchy", original_suffix,
-                                                 original_toolchain, toolchain_mapping[original_toolchain['name']])
+                                                 "hierarchy to %s toolchain hierarchy (versionsuffix mappings were %s)",
+                                                 original_suffix, original_toolchain,
+                                                 toolchain_mapping[original_toolchain['name']], versionsuffix_mappings)
                     else:
                         versionsuffix_mappings[original_suffix] = mapped_suffix
 
