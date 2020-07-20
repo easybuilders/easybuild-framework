d28ec4c892087ea26585f26f59487d47a6113401
Samuel Moors <samuel.moors@vub.be>
2020-07-20 17:14:01 +0200

    first try

--- easybuild/tools/options.py
+++ easybuild/tools/options.py
@@ -430,6 +430,8 @@ class EasyBuildOptions(GeneralOption):
             'silence-deprecation-warnings': ("Silence specified deprecation warnings", 'strlist', 'extend', None),
             'sticky-bit': ("Set sticky bit on newly created directories", None, 'store_true', False),
             'skip-test-cases': ("Skip running test cases", None, 'store_true', False, 't'),
+            'skip-make-devel-module': ("Skip creating a develop module file (useful for creating modules that cannot "
+                                       "be loaded)", None, 'store_true', False),
             'trace': ("Provide more information in output to stdout on progress", None, 'store_true', False, 'T'),
             'umask': ("umask to use (e.g. '022'); non-user write permissions on install directories are removed",
                       None, 'store', None),
