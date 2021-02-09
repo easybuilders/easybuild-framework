a3d980945cc22e6294f6a2f271c4e751ef68f7a7
Simon Branford <s.j.branford@bham.ac.uk>
2021-02-09 09:37:36 +0000

    Don't reuse variable name in the loop

--- easybuild/tools/toolchain/compiler.py
+++ easybuild/tools/toolchain/compiler.py
@@ -296,10 +296,10 @@ class Compiler(Toolchain):
                 self.variables.nextend(var, fflags)
             extra = 'extra_' + var.lower()
             if self.options.get(extra):
-                flags = self.options.option(extra)
-                if not flags or flags[0] != '-':
-                    raise EasyBuildError("toolchainopts %s: '%s' must start with a '-'." % (extra, flags))
-                self.variables.nappend_el(var, flags[1:])
+                extraflags = self.options.option(extra)
+                if not extraflags or extraflags[0] != '-':
+                    raise EasyBuildError("toolchainopts %s: '%s' must start with a '-'." % (extra, extraflags))
+                self.variables.nappend_el(var, extraflags[1:])
 
     def _set_optimal_architecture(self, default_optarch=None):
         """
