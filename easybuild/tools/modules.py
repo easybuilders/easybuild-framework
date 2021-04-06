8c7c16d981250e441eb4c2d11a34f9658da686e9
Alexander Grund <alexander.grund@tu-dresden.de>
2021-04-06 17:43:08 +0200

    Use mk_module_path

--- easybuild/tools/modules.py
+++ easybuild/tools/modules.py
@@ -2071,7 +2071,7 @@ class Lmod(ModulesTool):
                     new_mod_path = path
                 else:
                     new_mod_path = [path] + [p for p in cur_mod_path.split(':') if normalize_path(p) != path]
-                    new_mod_path = ':'.join(new_mod_path)
+                    new_mod_path = mk_module_path(new_mod_path)
                 self.log.debug('Changing MODULEPATH from %s to %s' %
                                ('<unset>' if cur_mod_path is None else cur_mod_path, new_mod_path))
                 os.environ['MODULEPATH'] = new_mod_path
@@ -2082,7 +2082,7 @@ class Lmod(ModulesTool):
         cur_mod_path = os.environ.get('MODULEPATH')
         if cur_mod_path is not None:
             path = normalize_path(path)
-            new_mod_path = ':'.join(p for p in cur_mod_path.split(':') if normalize_path(p) != path)
+            new_mod_path = mk_module_path(p for p in cur_mod_path.split(':') if normalize_path(p) != path)
             # Removing the last entry unsets the variable
             if not new_mod_path:
                 self.log.debug('Changing MODULEPATH from %s to <unset>' % cur_mod_path)
