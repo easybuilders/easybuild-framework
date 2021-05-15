ff2685636834e7c0ea93e4e48fdbd2b58fca7e20
Kenneth Hoste <kenneth.hoste@ugent.be>
2021-05-15 20:17:21 +0200

    update FlexiBLAS toolchain support to treat backends as banned libraries

--- easybuild/toolchains/linalg/flexiblas.py
+++ easybuild/toolchains/linalg/flexiblas.py
@@ -27,10 +27,38 @@ Support for FlexiBLAS as toolchain linear algebra library.
 
 :author: Kenneth Hoste (Ghent University)
 """
+import re
+
 from easybuild.tools.toolchain.linalg import LinAlg
+
+from easybuild.tools.run import run_cmd
+from easybuild.tools.systemtools import get_shared_lib_ext
 
 
 TC_CONSTANT_FLEXIBLAS = 'FlexiBLAS'
+
+
+def det_flexiblas_backend_libs():
+    """Determine list of paths to FlexiBLAS backend libraries."""
+
+    # example output for 'flexiblas list':
+    # System-wide (config directory):
+    #  OPENBLAS
+    #    library = libflexiblas_openblas.so
+    out, _ = run_cmd("flexiblas list", simple=False, trace=False)
+
+    shlib_ext = get_shared_lib_ext()
+    flexiblas_lib_regex = re.compile(r'library = (?P<lib>lib.*\.%s)' % shlib_ext, re.M)
+    flexiblas_libs = flexiblas_lib_regex.findall(out)
+
+    backend_libs = []
+    for flexiblas_lib in flexiblas_libs:
+        # assumption here is that the name of FlexiBLAS library (like 'libflexiblas_openblas.so')
+        # maps directly to name of the backend library ('libopenblas.so')
+        backend_lib = 'lib' + flexiblas_lib.lstrip('libflexiblas_')
+        backend_libs.append(backend_lib)
+
+    return backend_libs
 
 
 class FlexiBLAS(LinAlg):
@@ -44,3 +72,20 @@ class FlexiBLAS(LinAlg):
     LAPACK_MODULE_NAME = ['FlexiBLAS']
     LAPACK_IS_BLAS = True
     LAPACK_FAMILY = TC_CONSTANT_FLEXIBLAS
+
+    def __init__(self, *args, **kwargs):
+        """Constructor for FlexiBLAS toolchain component."""
+
+    @property
+    def banned_linked_shared_libs(self):
+        """
+        List of shared libraries (names, file names, paths) which are
+        not allowed to be linked in any installed binary/library.
+        """
+        banned_libs = super(FlexiBLAS, super).banned_linked_shared_libs
+
+        # register backends are banned shared libraries,
+        # to avoid that anything links to them directly (rather than to libflexiblas.so)
+        flexiblas_banned_libs = det_flexiblas_backend_libs()
+
+        return banned_libs + flexiblas_banned_libs
