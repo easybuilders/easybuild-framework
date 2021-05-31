e0eed8c0ece378f2a0904abae6a69f6bf5df4b71
Alexander Grund <alexander.grund@tu-dresden.de>
2021-05-31 16:17:16 +0200

    Add per-step timing information
    Shows e.g.:
    == fetching files...
    == creating build dir, resetting environment...
    == ... (took 2 secs)
    == unpacking...
    == ... (took 15 secs)
    == patching...
    == ... (took 7 secs)

--- easybuild/framework/easyblock.py
+++ easybuild/framework/easyblock.py
@@ -3528,7 +3528,13 @@ class EasyBlock(object):
                     else:
                         print_msg("%s..." % descr, log=self.log, silent=self.silent)
                     self.current_step = step_name
+                    start_time = datetime.now()
                     self.run_step(step_name, step_methods)
+                    step_duration = datetime.now() - start_time
+                    if step_duration.total_seconds() >= 1:
+                        print_msg("... (took %s)", time2str(step_duration))
+                    elif self.logdebug or build_option('trace'):
+                        print_msg("... (took < 1 sec)")
 
         except StopException:
             pass
