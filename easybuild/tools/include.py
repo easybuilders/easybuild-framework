0677bc210cf92fde37d5da0838e1635c3ae3f14c
Alexander Grund <alexander.grund@tu-dresden.de>
2021-07-09 14:52:52 +0200

    Conditionally delete

--- easybuild/tools/include.py
+++ easybuild/tools/include.py
@@ -72,7 +72,9 @@ subdirs = [chr(char) for char in range(ord('a'), ord('z') + 1)] + ['0']
 for subdir in subdirs:
     __path__ = pkgutil.extend_path(__path__, '%s.%s' % (__name__, subdir))
 
-del char, subdir, subdirs
+del subdir, subdirs
+if 'char' in dir():
+    del char
 
 __path__ = __import__('pkgutil').extend_path(__path__, __name__)
 """
