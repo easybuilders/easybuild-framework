a7bdee40a25687b3ebfcba0610a5407859e1bfdc
Alexander Grund <alexander.grund@tu-dresden.de>
2021-07-09 14:33:10 +0200

    Fix forgotten renaming of 'l' to 'char'

--- easybuild/tools/include.py
+++ easybuild/tools/include.py
@@ -72,7 +72,7 @@ subdirs = [chr(char) for char in range(ord('a'), ord('z') + 1)] + ['0']
 for subdir in subdirs:
     __path__ = pkgutil.extend_path(__path__, '%s.%s' % (__name__, subdir))
 
-del l, subdir, subdirs
+del char, subdir, subdirs
 
 __path__ = __import__('pkgutil').extend_path(__path__, __name__)
 """
