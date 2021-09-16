c19f7f1e49403fc5cbd29926d5567e09328a6084
Alexander Grund <alexander.grund@tu-dresden.de>
2021-09-16 15:26:01 +0200

    Add --ec option

--- easybuild/scripts/findPythonDeps.py
+++ easybuild/scripts/findPythonDeps.py
@@ -5,10 +5,22 @@ import json
 import os
 import pkg_resources
 import re
+import shutil
 import subprocess
 import sys
 import tempfile
+from contextlib import contextmanager
 from pprint import pprint
+
+
+@contextmanager
+def temporary_directory(*args, **kwargs):
+    """Resource wrapper over tempfile.mkdtemp"""
+    name = tempfile.mkdtemp(*args, **kwargs)
+    try:
+        yield name
+    finally:
+        shutil.rmtree(name)
 
 
 def extract_pkg_name(package_spec):
@@ -36,7 +48,7 @@ def run_in_venv(cmd, venv_path, action_desc):
 def get_dep_tree(package_spec):
     """Get the dep-tree for installing the given Python package spec"""
     package_name = extract_pkg_name(package_spec)
-    with tempfile.TemporaryDirectory(suffix=package_name + '-deps') as tmp_dir:
+    with temporary_directory(suffix=package_name + '-deps') as tmp_dir:
         # prevent pip from (ab)using $HOME/.cache/pip
         os.environ['XDG_CACHE_HOME'] = os.path.join(tmp_dir, 'pip-cache')
         venv_dir = os.path.join(tmp_dir, 'venv')
@@ -67,6 +79,37 @@ def find_deps(pkgs, dep_tree):
     return res
 
 
+def print_deps(package, verbose):
+    if verbose:
+        print('Getting dep tree of ' + package)
+    dep_tree = get_dep_tree(package)
+    if verbose:
+        print('Extracting dependencies of ' + package)
+    deps = find_deps([extract_pkg_name(package)], dep_tree)
+
+    installed_modules = {mod.project_name for mod in pkg_resources.working_set}
+    if verbose:
+        print("Installed modules: %s" % installed_modules)
+
+    # iterate over deps in reverse order, get rid of duplicates along the way
+    # also filter out Python packages that are already installed in current environment
+    res = []
+    handled = set()
+    for dep in reversed(deps):
+        if dep not in handled:
+            handled.add(dep)
+            if dep[0] in installed_modules:
+                if verbose:
+                    print("Skipping installed module '%s'" % dep[0])
+            else:
+                res.append(dep)
+
+    print("List of dependencies in (likely) install order:")
+    pprint(res, indent=4)
+    print("Sorted list of dependencies:")
+    pprint(sorted(res), indent=4)
+
+
 parser = argparse.ArgumentParser(
     description='Find dependencies of Python packages by installing it in a temporary virtualenv. ',
     epilog=' && '.join(['Example usage with EasyBuild: '
@@ -76,34 +119,25 @@ parser = argparse.ArgumentParser(
 )
 parser.add_argument('package', metavar='python-pkg-spec',
                     help='Python package spec, e.g. tensorflow==2.3.4')
+parser.add_argument('--ec', metavar='easyconfig', help='EasyConfig to use as the build environment')
 parser.add_argument('--verbose', help='Verbose output', action='store_true')
 args = parser.parse_args()
 
-if args.verbose:
-    print('Getting dep tree of ' + args.package)
-dep_tree = get_dep_tree(args.package)
-if args.verbose:
-    print('Extracting dependencies of ' + args.package)
-deps = find_deps([extract_pkg_name(args.package)], dep_tree)
+if args.ec:
+    with temporary_directory() as tmp_dir:
+        old_dir = os.getcwd()
+        os.chdir(tmp_dir)
+        if args.verbose:
+            print('Running EasyBuild to get build environment')
+        run_cmd(['eb', args.ec, '--dump-env', '--force'], action_desc='Dump build environment')
+        os.chdir(old_dir)
 
-installed_modules = {mod.project_name for mod in pkg_resources.working_set}
-if args.verbose:
-    print("Installed modules: %s" % installed_modules)
+        cmd = 'source %s/*.env && %s "%s"' % (tmp_dir, sys.argv[0], args.package)
+        if args.verbose:
+            cmd += ' --verbose'
+            print('Restarting script in new build environment')
 
-# iterate over deps in reverse order, get rid of duplicates along the way
-# also filter out Python packages that are already installed in current environment
-res = []
-handled = set()
-for dep in reversed(deps):
-    if dep not in handled:
-        handled.add(dep)
-        if dep[0] in installed_modules:
-            if args.verbose:
-                print("Skipping installed module '%s'" % dep[0])
-        else:
-            res.append(dep)
-
-print("List of dependencies in (likely) install order:")
-pprint(res, indent=4)
-print("Sorted list of dependencies:")
-pprint(sorted(res), indent=4)
+        out = run_cmd(cmd, action_desc='Run in new environment', shell=True, executable='/bin/bash')
+        print(out)
+else:
+    print_deps(args.package, args.verbose)
