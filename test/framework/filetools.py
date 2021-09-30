c273b105df0c28be0845daed390cc14a7e7d709f
Ake Sandgren <ake.sandgren@hpc2n.umu.se>
2021-09-30 10:44:30 +0200

    Forgot to update the check of the number of .eb files in the test_ecs tree after adding the test-link-0.0.eb

--- test/framework/filetools.py
+++ test/framework/filetools.py
@@ -2203,7 +2203,7 @@ class FileToolsTest(EnhancedTestCase):
         # test with specified path with and without trailing '/'s
         for path in [test_ecs, test_ecs + '/', test_ecs + '//']:
             index = ft.create_index(path)
-            self.assertEqual(len(index), 89)
+            self.assertEqual(len(index), 90)
 
             expected = [
                 os.path.join('b', 'bzip2', 'bzip2-1.0.6-GCC-4.9.2.eb'),
