fbb72469f139ec4255c79b4e21f9a5b0d99daceb
Kenneth Hoste <kenneth.hoste@ugent.be>
2023-06-30 10:40:46 +0200

    fix regex for extracting glibc version from output of 'ldd --version' in Gentoo Linux

--- easybuild/tools/systemtools.py
+++ easybuild/tools/systemtools.py
@@ -943,7 +943,7 @@ def get_glibc_version():
 
     if os_type == LINUX:
         glibc_ver_str = get_tool_version('ldd')
-        glibc_ver_regex = re.compile(r"^ldd \([^)]*\) (\d[\d.]*).*$")
+        glibc_ver_regex = re.compile(r"^ldd \(.+\) (\d[\d.]+);")
         res = glibc_ver_regex.search(glibc_ver_str)
 
         if res is not None:
