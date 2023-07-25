a80ac7f57f8b6728e104ff8d3326f63bcd94668a
XavierCS-dev <xavierelwell@gmail.com>
2023-07-25 14:17:10 +0100

    Init eb_go with parse_options()

--- easybuild/main.py
+++ easybuild/main.py
@@ -71,7 +71,7 @@ from easybuild.tools.github import new_pr_from_branch
 from easybuild.tools.github import sync_branch_with_develop, sync_pr_with_develop, update_branch, update_pr
 from easybuild.tools.hooks import START, END, CANCEL, FAIL, load_hooks, run_hook
 from easybuild.tools.modules import modules_tool
-from easybuild.tools.options import opts_dict_to_eb_opts, set_up_configuration, use_color
+from easybuild.tools.options import opts_dict_to_eb_opts, parse_options, set_up_configuration, use_color
 from easybuild.tools.output import COLOR_GREEN, COLOR_RED, STATUS_BAR, colorize, print_checks, rich_live_cm
 from easybuild.tools.output import start_progress_bar, stop_progress_bar, update_progress_bar
 from easybuild.tools.robot import check_conflicts, dry_run, missing_deps, resolve_dependencies, search_easyconfigs
@@ -730,7 +730,7 @@ def main(args=None, logfile=None, do_build=None, testing=False, modtool=None):
 
 
 if __name__ == "__main__":
-    eb_go, _ = set_up_configuration(args=None, logfile=None, testing=None)
+    eb_go = parse_options()
     hooks = load_hooks(eb_go.options.hooks)
     try:
         main()
