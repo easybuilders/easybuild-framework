8a474bb8ad69d756c7c606fcd3dc80c76a382c16
XavierCS-dev <xavierelwell@gmail.com>
2023-08-10 10:55:32 +0100

    Fix output tests for hook order

--- test/framework/options.py
+++ test/framework/options.py
@@ -719,9 +719,9 @@ class CommandLineOptionsTest(EnhancedTestCase):
             "	post_testcases_hook",
             "	post_build_and_install_loop_hook",
             "	end_hook",
-            "	fail_hook",
             "	cancel_hook",
             "	crash_hook",
+            "	fail_hook",
             '',
         ])
         self.assertEqual(stdout, expected)
