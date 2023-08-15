886d7bf2bb7bc76902edae69ed67b78af695c01d
Alexander Grund <alexander.grund@tu-dresden.de>
2023-08-15 16:26:30 +0200

    Add `makedirs(..., exits_ok)` for Python2

--- easybuild/tools/py2vs3/py2.py
+++ easybuild/tools/py2vs3/py2.py
@@ -33,8 +33,10 @@ Authors:
 """
 # these are not used here, but imported from here in other places
 import ConfigParser as configparser  # noqa
+import errno
 import imp
 import json
+import os
 import subprocess
 import time
 import urllib2 as std_urllib  # noqa
@@ -115,3 +117,11 @@ def sort_looseversions(looseversions):
     # with Python 2, we can safely use 'sorted' on LooseVersion instances
     # (but we can't in Python 3, see https://bugs.python.org/issue14894)
     return sorted(looseversions)
+
+
+def makedirs(name, mode=0o777, exist_ok=False):
+    try:
+        os.makedirs(name, mode)
+    except OSError as e:
+        if not exist_ok or e.errno != errno.EEXIST:
+            raise
