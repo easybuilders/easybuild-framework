ad3d411e80adb91dfa2bd754aca1dc5eb1f2b80b
Kenneth Hoste <kenneth.hoste@ugent.be>
2023-08-30 14:40:28 +0200

    improve docstring for validate_github_token to cover both classic and fine-grained GitHub tokens

--- easybuild/tools/github.py
+++ easybuild/tools/github.py
@@ -2243,7 +2243,8 @@ def install_github_token(github_user, silent=False):
 def validate_github_token(token, github_user):
     """
     Check GitHub token:
-    * see if it conforms expectations (character classes depending on type, length of 40-93),
+    * see if it conforms expectations (classic GitHub token with only [0-9a-f] characters and length of 40 starting with 'ghp_'),
+      or fine-grained GitHub token with only alphanumeric ([a-zA-Z0-9]) + '_' and length of 93 starting with 'github_pat_'),
     * see if it can be used for authenticated access.
     """
     # cfr. https://github.blog/2021-04-05-behind-githubs-new-authentication-token-formats/
