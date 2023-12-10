a43ede26d84672bddd41c2f00247cbc7b8ec4df8
sam <sammoors@gmail.com>
2023-12-10 14:03:09 +0100

    also run test_cases_step with --sanity-check-only

--- easybuild/framework/easyblock.py
+++ easybuild/framework/easyblock.py
@@ -3860,6 +3860,10 @@ class EasyBlock(object):
         """
         Run provided test cases.
         """
+        # ensure parallel is set in case check_readiness_step is skipped (e.g. with --sanity-check-only)
+        if not self.cfg['parallel']:
+            self.set_parallel()
+
         for test in self.cfg['tests']:
             change_dir(self.orig_workdir)
             if os.path.isabs(test):
@@ -3915,7 +3919,7 @@ class EasyBlock(object):
             self.log.info("Skipping %s step because of forced module-only mode", step)
             skip = True
 
-        elif sanity_check_only and step != SANITYCHECK_STEP:
+        elif sanity_check_only and step not in [SANITYCHECK_STEP, TESTCASES_STEP]:
             self.log.info("Skipping %s step because of sanity-check-only mode", step)
             skip = True
 
