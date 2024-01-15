289ff54b64b90afce798a9d8bdf34b75251f1009
Simon Branford <4967+branfosj@users.noreply.github.com>
2024-01-15 16:12:18 +0000

    update deprecation message

--- easybuild/tools/modules.py
+++ easybuild/tools/modules.py
@@ -272,11 +272,7 @@ class ModulesTool(object):
                 if StrictVersion(self.version) < StrictVersion(self.DEPR_VERSION):
                     depr_msg = "Support for %s version < %s is deprecated, " % (self.NAME, self.DEPR_VERSION)
                     depr_msg += "found version %s" % self.version
-
-                    if self.version.startswith('6') and 'Lmod6' in build_option('silence_deprecation_warnings'):
-                        self.log.warning(depr_msg)
-                    else:
-                        self.log.deprecated(depr_msg, '5.0')
+                    self.log.deprecated(depr_msg, '6.0')
 
             if self.MAX_VERSION is not None:
                 self.log.debug("Maximum allowed %s version defined: %s", self.NAME, self.MAX_VERSION)
