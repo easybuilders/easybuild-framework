5202f07081133fbe55510664b885452eb8f7675e
Simon Branford <4967+branfosj@users.noreply.github.com>
2024-04-27 16:37:16 +0200

    " and ' mixing

--- easybuild/main.py
+++ easybuild/main.py
@@ -621,7 +621,7 @@ def main(args=None, logfile=None, do_build=None, testing=False, modtool=None, pr
 
     # compare running Framework and EasyBlocks versions
     if 'UNKNOWN' in EASYBLOCKS_VERSION:
-        print_msg('Unable to determine EasyBlocks version, so we'll assume it is not different from Framework')
+        print_msg("Unable to determine EasyBlocks version, so we'll assume it is not different from Framework")
     elif different_major_versions(FRAMEWORK_VERSION, EASYBLOCKS_VERSION):
         raise EasyBuildError("Framework (%s) and EasyBlock (%s) major versions are different." % (FRAMEWORK_VERSION,
                                                                                                   EASYBLOCKS_VERSION))
