cc902931c0b59172446c952a496cd546421fe5d0
Alex Domingo <alex.domingo.toro@vub.be>
2024-05-03 16:28:51 +0200

    easyconfig.tools.run is too low to use higher level method from filetools
    This reverts commit 85ef1dec85f02a383cd1a6320d5cccf21ffbaf92.

--- easybuild/tools/run.py
+++ easybuild/tools/run.py
@@ -65,7 +65,6 @@ except ImportError:
 from easybuild.base import fancylogger
 from easybuild.tools.build_log import EasyBuildError, dry_run_msg, print_msg, time_str_since
 from easybuild.tools.config import build_option
-from easybuild.tools.filetools import get_cwd
 from easybuild.tools.hooks import RUN_SHELL_CMD, load_hooks, run_hook
 from easybuild.tools.utilities import trace_msg
 
@@ -316,7 +315,7 @@ def run_shell_cmd(cmd, fail_on_error=True, split_stderr=False, stdin=None, env=N
         qa_wait_patterns = []
 
     if work_dir is None:
-        work_dir = get_cwd()
+        work_dir = os.getcwd()
 
     cmd_str = to_cmd_str(cmd)
 
