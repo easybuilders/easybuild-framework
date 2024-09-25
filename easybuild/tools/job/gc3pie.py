e40aba3f4f0a0ce2f37ba14c7b33527a724859e1
Georgios Kafanas <g.kafanas@greeklug.gr>
2024-09-25 05:13:16 +0200

    [refactor:job_backend] Use Slurm as the default job back-end
    - The GC3Pie back-end for job management that has been the default so far
      is no longer actively supported. Use the Slurm back-end as default
      instead.
    - Add deprecation message when the GC3Pie class is instantiated.
    
    Issue: https://github.com/easybuilders/easybuild-framework/issues/4565

--- easybuild/tools/job/gc3pie.py
+++ easybuild/tools/job/gc3pie.py
@@ -104,6 +104,10 @@ class GC3Pie(JobBackend):
     def _check_version(self):
         """Check whether GC3Pie version complies with required version."""
 
+        deprication_msg = "The GC3Pie job back-end is no longer maintained and will be deprecated"
+        deprication_msg += ", please use a different backend"
+        _log.deprecated(deprication_msg, '5.0')
+
         try:
             from pkg_resources import get_distribution, DistributionNotFound
             pkg = get_distribution('gc3pie')
