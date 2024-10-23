2aa779093bf78285f1dfdb523f6782442a25982c
Mikael Öhman <micketeer@gmail.com>
2024-10-23 10:51:37 +0200

    Add delimiter test for modextrapaths

--- test/framework/easyblock.py
+++ test/framework/easyblock.py
@@ -1259,6 +1259,7 @@ class EasyBlockTest(EnhancedTestCase):
         modextrapaths = {
             'PATH': ('xbin', 'pibin'),
             'CPATH': 'pi/include',
+            'TCLLIBPATH': {'path': 'pi', 'delimiter': ' '},
         }
         modextrapaths_append = {'APPEND_PATH': 'append_path'}
         self.contents = '\n'.join([
