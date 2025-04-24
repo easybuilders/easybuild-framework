5f3b4d15dcc61efa72fc5cce68c629cf5795ce64
Alex Domingo <alex.domingo.toro@vub.be>
2025-04-24 13:49:46 +0200

    fix formatting of inline comments in test_env

--- test/framework/module_generator.py
+++ test/framework/module_generator.py
@@ -955,34 +955,34 @@ class ModuleGeneratorTest(EnhancedTestCase):
         """Test setting of environment variables."""
         collection = (
             # value,               relpath, Tcl reference,                                    Lua reference
-            ("value",              False,   'setenv\tkey\t\t"value"\n',                       'setenv("key", "value")\n'),  #noqa
-            ('va"lue',             False,   'setenv\tkey\t\t"va\\"lue"\n',                    'setenv("key", \'va"lue\')\n'),  #noqa
-            ("va'lue",             False,   'setenv\tkey\t\t"va\'lue"\n',                     'setenv("key", "va\'lue")\n'),  #noqa
-            ("""va"l'ue""",        False,   'setenv\tkey\t\t"""va"l\'ue"""\n',                'setenv("key", """va"l\'ue""")\n'),  #noqa
-            ("relative/path",      False,   'setenv\tkey\t\t"relative/path"\n',               'setenv("key", "relative/path")\n'),  #noqa
-            ("relative/path",      True,    'setenv\tkey\t\t"$root/relative/path"\n',         'setenv("key", pathJoin(root, "relative", "path"))\n'),  #noqa
-            ("relative/path/",     False,   'setenv\tkey\t\t"relative/path/"\n',              'setenv("key", "relative/path/")\n'),  #noqa
-            ("relative/path/",     True,    'setenv\tkey\t\t"$root/relative/path/"\n',        'setenv("key", pathJoin(root, "relative", "path"))\n'),  #noqa
-            ("",                   False,   'setenv\tkey\t\t""\n',                            'setenv("key", "")\n'),  #noqa
-            ("",                   True,    'setenv\tkey\t\t"$root"\n',                       'setenv("key", root)\n'),  #noqa
-            ("/absolute/path",     False,   'setenv\tkey\t\t"/absolute/path"\n',              'setenv("key", "/absolute/path")\n'),  #noqa
-            ("/absolute/path",     True,    'setenv\tkey\t\t"/absolute/path"\n',              'setenv("key", pathJoin("/", "absolute", "path"))\n'),  #noqa
-            ("/",                  False,   'setenv\tkey\t\t"/"\n',                           'setenv("key", "/")\n'),  #noqa
-            ("/",                  True,    'setenv\tkey\t\t"/"\n',                           'setenv("key", "/")\n'),  #noqa
-            ("$VAR",               False,   'setenv\tkey\t\t"$::env(VAR)"\n',                 'setenv("key", os.getenv("VAR"))\n'),  #noqa
-            ("$VAR",               True,    'setenv\tkey\t\t"$root/$::env(VAR)"\n',           'setenv("key", pathJoin(root, os.getenv("VAR")))\n'),  #noqa
-            ("$VAR/in/path",       False,   'setenv\tkey\t\t"$::env(VAR)/in/path"\n',         'setenv("key", os.getenv("VAR") .. "/in/path")\n'),  #noqa
-            ("$VAR/in/path",       True,    'setenv\tkey\t\t"$root/$::env(VAR)/in/path"\n',   'setenv("key", pathJoin(root, os.getenv("VAR"), "in", "path"))\n'),  #noqa
-            ("path/with/$VAR",     False,   'setenv\tkey\t\t"path/with/$::env(VAR)"\n',       'setenv("key", "path/with/" .. os.getenv("VAR"))\n'),  #noqa
-            ("path/with/$VAR",     True,    'setenv\tkey\t\t"$root/path/with/$::env(VAR)"\n', 'setenv("key", pathJoin(root, "path", "with", os.getenv("VAR")))\n'),  #noqa
-            ("path/$VAR/dir",      False,   'setenv\tkey\t\t"path/$::env(VAR)/dir"\n',        'setenv("key", "path/" .. os.getenv("VAR") .. "/dir")\n'),  #noqa
-            ("path/$VAR/dir",      True,    'setenv\tkey\t\t"$root/path/$::env(VAR)/dir"\n',  'setenv("key", pathJoin(root, "path", os.getenv("VAR"), "dir"))\n'),  #noqa
-            ("/$VAR/in/abspath",   False,   'setenv\tkey\t\t"/$::env(VAR)/in/abspath"\n',     'setenv("key", "/" .. os.getenv("VAR") .. "/in/abspath")\n'),  #noqa
-            ("/$VAR/in/abspath",   True,    'setenv\tkey\t\t"/$::env(VAR)/in/abspath"\n',     'setenv("key", pathJoin("/", os.getenv("VAR"), "in", "abspath"))\n'),  #noqa
-            ("/abspath/with/$VAR", False,   'setenv\tkey\t\t"/abspath/with/$::env(VAR)"\n',   'setenv("key", "/abspath/with/" .. os.getenv("VAR"))\n'),  #noqa
-            ("/abspath/with/$VAR", True,    'setenv\tkey\t\t"/abspath/with/$::env(VAR)"\n',   'setenv("key", pathJoin("/", "abspath", "with", os.getenv("VAR")))\n'),  #noqa
-            ("/abspath/$VAR/dir",  False,   'setenv\tkey\t\t"/abspath/$::env(VAR)/dir"\n',    'setenv("key", "/abspath/" .. os.getenv("VAR") .. "/dir")\n'),  #noqa
-            ("/abspath/$VAR/dir",  True,    'setenv\tkey\t\t"/abspath/$::env(VAR)/dir"\n',    'setenv("key", pathJoin("/", "abspath", os.getenv("VAR"), "dir"))\n'),  #noqa
+            ("value",              False,   'setenv\tkey\t\t"value"\n',                       'setenv("key", "value")\n'),  # noqa
+            ('va"lue',             False,   'setenv\tkey\t\t"va\\"lue"\n',                    'setenv("key", \'va"lue\')\n'),  # noqa
+            ("va'lue",             False,   'setenv\tkey\t\t"va\'lue"\n',                     'setenv("key", "va\'lue")\n'),  # noqa
+            ("""va"l'ue""",        False,   'setenv\tkey\t\t"""va"l\'ue"""\n',                'setenv("key", """va"l\'ue""")\n'),  # noqa
+            ("relative/path",      False,   'setenv\tkey\t\t"relative/path"\n',               'setenv("key", "relative/path")\n'),  # noqa
+            ("relative/path",      True,    'setenv\tkey\t\t"$root/relative/path"\n',         'setenv("key", pathJoin(root, "relative", "path"))\n'),  # noqa
+            ("relative/path/",     False,   'setenv\tkey\t\t"relative/path/"\n',              'setenv("key", "relative/path/")\n'),  # noqa
+            ("relative/path/",     True,    'setenv\tkey\t\t"$root/relative/path/"\n',        'setenv("key", pathJoin(root, "relative", "path"))\n'),  # noqa
+            ("",                   False,   'setenv\tkey\t\t""\n',                            'setenv("key", "")\n'),  # noqa
+            ("",                   True,    'setenv\tkey\t\t"$root"\n',                       'setenv("key", root)\n'),  # noqa
+            ("/absolute/path",     False,   'setenv\tkey\t\t"/absolute/path"\n',              'setenv("key", "/absolute/path")\n'),  # noqa
+            ("/absolute/path",     True,    'setenv\tkey\t\t"/absolute/path"\n',              'setenv("key", pathJoin("/", "absolute", "path"))\n'),  # noqa
+            ("/",                  False,   'setenv\tkey\t\t"/"\n',                           'setenv("key", "/")\n'),  # noqa
+            ("/",                  True,    'setenv\tkey\t\t"/"\n',                           'setenv("key", "/")\n'),  # noqa
+            ("$VAR",               False,   'setenv\tkey\t\t"$::env(VAR)"\n',                 'setenv("key", os.getenv("VAR"))\n'),  # noqa
+            ("$VAR",               True,    'setenv\tkey\t\t"$root/$::env(VAR)"\n',           'setenv("key", pathJoin(root, os.getenv("VAR")))\n'),  # noqa
+            ("$VAR/in/path",       False,   'setenv\tkey\t\t"$::env(VAR)/in/path"\n',         'setenv("key", os.getenv("VAR") .. "/in/path")\n'),  # noqa
+            ("$VAR/in/path",       True,    'setenv\tkey\t\t"$root/$::env(VAR)/in/path"\n',   'setenv("key", pathJoin(root, os.getenv("VAR"), "in", "path"))\n'),  # noqa
+            ("path/with/$VAR",     False,   'setenv\tkey\t\t"path/with/$::env(VAR)"\n',       'setenv("key", "path/with/" .. os.getenv("VAR"))\n'),  # noqa
+            ("path/with/$VAR",     True,    'setenv\tkey\t\t"$root/path/with/$::env(VAR)"\n', 'setenv("key", pathJoin(root, "path", "with", os.getenv("VAR")))\n'),  # noqa
+            ("path/$VAR/dir",      False,   'setenv\tkey\t\t"path/$::env(VAR)/dir"\n',        'setenv("key", "path/" .. os.getenv("VAR") .. "/dir")\n'),  # noqa
+            ("path/$VAR/dir",      True,    'setenv\tkey\t\t"$root/path/$::env(VAR)/dir"\n',  'setenv("key", pathJoin(root, "path", os.getenv("VAR"), "dir"))\n'),  # noqa
+            ("/$VAR/in/abspath",   False,   'setenv\tkey\t\t"/$::env(VAR)/in/abspath"\n',     'setenv("key", "/" .. os.getenv("VAR") .. "/in/abspath")\n'),  # noqa
+            ("/$VAR/in/abspath",   True,    'setenv\tkey\t\t"/$::env(VAR)/in/abspath"\n',     'setenv("key", pathJoin("/", os.getenv("VAR"), "in", "abspath"))\n'),  # noqa
+            ("/abspath/with/$VAR", False,   'setenv\tkey\t\t"/abspath/with/$::env(VAR)"\n',   'setenv("key", "/abspath/with/" .. os.getenv("VAR"))\n'),  # noqa
+            ("/abspath/with/$VAR", True,    'setenv\tkey\t\t"/abspath/with/$::env(VAR)"\n',   'setenv("key", pathJoin("/", "abspath", "with", os.getenv("VAR")))\n'),  # noqa
+            ("/abspath/$VAR/dir",  False,   'setenv\tkey\t\t"/abspath/$::env(VAR)/dir"\n',    'setenv("key", "/abspath/" .. os.getenv("VAR") .. "/dir")\n'),  # noqa
+            ("/abspath/$VAR/dir",  True,    'setenv\tkey\t\t"/abspath/$::env(VAR)/dir"\n',    'setenv("key", pathJoin("/", "abspath", os.getenv("VAR"), "dir"))\n'),  # noqa
         )
         # test set_environment
         for test_value, test_relpath, ref_tcl, ref_lua in collection:
