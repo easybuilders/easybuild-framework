3b447698278346c60aecb94dde14fd97c8d06876
Pavel Tomanek <99190809+pavelToman@users.noreply.github.com>
2025-05-22 14:36:26 +0200

    Update easyblock.py

--- easybuild/framework/easyblock.py
+++ easybuild/framework/easyblock.py
@@ -3734,12 +3734,21 @@ class EasyBlock:
 
         return fail_msgs
 
-    def sanity_check_rpath(self, rpath_dirs=None, check_readelf_rpath=True):
+    def sanity_check_rpath(self, rpath_dirs=None, check_readelf_rpath=None):
         """Sanity check binaries/libraries w.r.t. RPATH linking."""
 
         self.log.info("Checking RPATH linkage for binaries/libraries...")
 
         fails = []
+        
+        # Configure RPATH checking by readelf using easyconfig variable 'check_readelf_rpath' (default True)
+        if check_readelf_rpath is None:
+            check_readelf_rpath = self.cfg["check_readelf_rpath"]
+            (
+                self.log.info("RPATH checking by readelf is enabled")
+                if check_readelf_rpath
+                else self.log.info("RPATH checking by readelf is disabled")
+            )
 
         if build_option('strict_rpath_sanity_check'):
             self.log.info("Unsetting $LD_LIBRARY_PATH since strict RPATH sanity check is enabled...")
