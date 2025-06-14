dd04f4fb973497c94e23c99afd11ecda8bee52cf
ocaisa <alan.ocais@cecam.org>
2025-06-14 21:38:28 +0200

    Update test/framework/toy_build.py

--- test/framework/toy_build.py
+++ test/framework/toy_build.py
@@ -3662,7 +3662,7 @@ class ToyBuildTest(EnhancedTestCase):
                print(ec['postinstallcmds'][-1])
 
             def pre_build_and_install_loop_hook(ecs):
-                print(f'installing {len(ecs)} easyconfigs: {' '.join(ec['full_mod_name'] for ec in ecs)}')
+                print(f"installing {len(ecs)} easyconfigs: {' '.join(ec['full_mod_name'] for ec in ecs)}")
 
             def pre_easyblock_hook(self):
                 print(f'starting installation of {self.name} {self.version}')
