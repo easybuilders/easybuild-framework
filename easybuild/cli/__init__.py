50ba676c0f505e1f968413341447cf7a704b150a
crivella <davide.grassano@epfl.ch>
2025-07-31 13:58:21 +0200

    Passthrough the arguments from the CLI to optparse instead of rebuilding the,

--- easybuild/cli/__init__.py
+++ easybuild/cli/__init__.py
@@ -1,4 +1,3 @@
-import os
 import sys
 
 try:
@@ -31,61 +30,9 @@ else:
 
     @click.command()
     @EasyBuildCliOption.apply_options
-    @click.pass_context
     @click.argument('other_args', nargs=-1, type=EasyconfigParam(), required=False)
-    def eb(ctx, other_args):
+    def eb(other_args):
         """EasyBuild command line interface."""
-        args = []
-        for key, value in getattr(ctx, 'hidden_params', {}).items():
-            key = key.replace('_', '-')
-            opt = EasyBuildCliOption.OPTIONS_MAP[key]
-            if value in ['False', 'True']:
-                value = value == 'True'
-            if isinstance(value, bool):
-                if value != opt.default:
-                    if value:
-                        args.append(f"--{key}")
-                    else:
-                        args.append(f"--disable-{key}")
-            else:
-
-                if isinstance(value, (list, tuple)) and value:
-                    # Flatten nested lists if necessary
-                    if isinstance(value[0], list):
-                        value = sum(value, [])
-                # Match the type of the option with the default to see if we need to add it
-                if value and isinstance(value, list) and isinstance(opt.default, tuple):
-                    value = tuple(value)
-                if value and isinstance(value, tuple) and isinstance(opt.default, list):
-                    value = list(value)
-                value_is_default = (value == opt.default)
-
-                value_flattened = value
-                if isinstance(value, (list, tuple)):
-                    if 'path' in opt.type:
-                        delim = os.pathsep
-                    elif 'str' in opt.type:
-                        delim = ','
-                    elif 'url' in opt.type:
-                        delim = '|'
-                    else:
-                        raise ValueError(f"Unsupported type for {key}: {opt.type}")
-                    value_flattened = delim.join(value)
-
-                if opt.action == 'store_or_None':
-                    if value is None or value == ():
-                        continue
-                    if value_is_default:
-                        args.append(f"--{key}")
-                    else:
-                        args.append(f"--{key}={value_flattened}")
-                elif value and not value_is_default:
-                    if value:
-                        args.append(f"--{key}={value_flattened}")
-                    else:
-                        args.append(f"--{key}")
-        for arg in args:
-            print(f"ARG: {arg}")
-
-        args.extend(other_args)
-        main_with_hooks(args=args)
+        # Really no need to re-build the arguments if we support the exact same syntax we can just let them pass
+        # through to optparse
+        main_with_hooks()
