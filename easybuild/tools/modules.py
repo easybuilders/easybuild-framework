549c4ec330e01a2292393f0b4455e9ce3669781a
Alexander Grund <alexander.grund@tu-dresden.de>
2025-11-14 16:01:52 +0100

    Dump command env for module commands in debug mode

--- easybuild/tools/modules.py
+++ easybuild/tools/modules.py
@@ -1259,12 +1259,13 @@ class ModulesTool:
                 self.log.debug("Changing %s from '%s' to '%s' in environment for module command",
                                key, old_value, new_value)
 
-        log_output = build_option('debug_module_cmds') and not kwargs.get('hide_output', False)
+        debug_module_cmds = build_option('debug_module_cmds')
+        log_output = debug_module_cmds and not kwargs.get('hide_output', False)
         cmd_list = self.compose_cmd_list(args)
         cmd = ' '.join(cmd_list)
         # note: module commands are always run in dry mode, and are kept hidden in trace and dry run output
         res = run_shell_cmd(cmd_list, env=environ, fail_on_error=False, use_bash=False, split_stderr=True,
-                            hidden=True, in_dry_run=True, output_file=False,
+                            hidden=True, in_dry_run=True, output_file=debug_module_cmds,
                             log_output_on_success=log_output)
 
         # stdout will contain python code (to change environment etc)
