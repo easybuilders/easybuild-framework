1b9bc7bb3cd23e0454b33494c9138a3455bf7327
Alex Domingo <alex.domingo.toro@vub.be>
2026-01-20 16:28:29 +0100

    allow deprecated behaviour on tests handling toolchains

--- test/framework/options.py
+++ test/framework/options.py
@@ -833,7 +833,7 @@ class CommandLineOptionsTest(EnhancedTestCase):
             '--list-toolchains',
             '--unittest-file=%s' % self.logfile,
         ]
-        with self.mocked_stdout_stderr():
+        with self.temporarily_allow_deprecated_behaviour(), self.mocked_stdout_stderr():
             self.eb_main(args, logfile=dummylogfn, raise_error=True)
 
         logtxt = read_file(self.logfile)
@@ -863,11 +863,12 @@ class CommandLineOptionsTest(EnhancedTestCase):
             '--list-toolchains',
             '--output-format=rst',
         ]
-        with self.mocked_stdout_stderr():
+        with self.temporarily_allow_deprecated_behaviour(), self.mocked_stdout_stderr():
             self.eb_main(args, raise_error=True)
             stderr, stdout = self.get_stderr(), self.get_stdout().strip()
 
-        self.assertFalse(stderr)
+        if "WARNING: Deprecated" not in stderr:
+            self.assertFalse(stderr)
 
         title = "List of known toolchains"
 
@@ -4156,7 +4157,7 @@ class CommandLineOptionsTest(EnhancedTestCase):
         # TestIncludedCompiler is not available by default
         args = ['--list-toolchains']
         test_cmd = self.mk_eb_test_cmd(args)
-        with self.mocked_stdout_stderr():
+        with self.temporarily_allow_deprecated_behaviour(), self.mocked_stdout_stderr():
             res = run_shell_cmd(test_cmd)
         self.assertNotRegex(res.output, tc_regex)
 
@@ -4178,7 +4179,7 @@ class CommandLineOptionsTest(EnhancedTestCase):
 
         args.append('--include-toolchains=%s/*.py,%s/*/*.py' % (self.test_prefix, self.test_prefix))
         test_cmd = self.mk_eb_test_cmd(args)
-        with self.mocked_stdout_stderr():
+        with self.temporarily_allow_deprecated_behaviour(), self.mocked_stdout_stderr():
             res = run_shell_cmd(test_cmd)
         self.assertRegex(res.output, tc_regex)
 
