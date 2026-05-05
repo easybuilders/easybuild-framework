e2c076e4a0ca82d815c72565184b743b95c66638
Alexander Grund <alexander.grund@tu-dresden.de>
2026-05-05 13:07:13 +0200

    Use `assert(Not)Regex` instead of `assertTrue` or `assertFalse` in tests
    This makes the tests shorter and hence easier to read, avoids
    `re.compile` calls and manual failure message composing.
    In some cases `assert_multi_regex` could be used to avoid the explicit loops.

--- test/framework/options.py
+++ test/framework/options.py
@@ -6985,8 +6985,9 @@ class CommandLineOptionsTest(EnhancedTestCase):
         """Test --cuda-compute-capabilities configuration option."""
         args = ['--cuda-compute-capabilities=3.5,6.2,7.0,9.0a,10.0f', '--show-config']
         txt, _ = self._run_mock_eb(args, do_build=True, raise_error=True, testing=False, strip=True)
-        regex = re.compile(r"^cuda-compute-capabilities\s*\(C\)\s*=\s*3\.5, 6\.2, 7\.0, 9\.0a, 10\.0f$", re.M)
-        self.assertRegex(txt, regex)
+        self.assertRegex(txt,
+                         re.compile(r"^cuda-compute-capabilities\s*\(C\)\s*=\s*3\.5, 6\.2, 7\.0, 9\.0a, 10\.0f$",
+                                    re.M))
 
     def test_create_index(self):
         """Test --create-index option."""
