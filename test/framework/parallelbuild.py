28168ca77037db54234e882d9833b0bb0dcf12f7
Xavier Besseron <xavier.besseron@lxp.lu>
2026-06-16 14:41:37 +0200

    Fix mocked sacct command

--- test/framework/parallelbuild.py
+++ test/framework/parallelbuild.py
@@ -79,7 +79,7 @@ MOCKED_SCONTROL = """#!/bin/bash
 """
 
 # Mock the output of sacct.
-# Expected call to sacct: 
+# Expected call to sacct:
 # `sacct --allocations --noheader --parsable2 --jobs=1,2,3,4 --format=JobID,State,Elapsed`
 # Expected output:
 # ```
@@ -92,7 +92,7 @@ MOCKED_SACCT = """#!/bin/bash
     # We write the actual command line parameters to stderr
     echo "(sacct args: $@)" >&2
     # For each job ID, we write to stdout a line showing the job as completed.
-    for JOBID in $(echo "$*" | sed 's/^.*--jobs=\([^ ]*\) .*$/\1/;s/,/ /g') ; do
+    for JOBID in $(echo "$*" | sed 's/^.*--jobs=\\([^ ]*\\) .*$/\1/;s/,/ /g') ; do
        echo "${JOBID}|COMPLETED|00:01:00"
     done
 """
