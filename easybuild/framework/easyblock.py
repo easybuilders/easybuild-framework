1cef48978757e36bede798ba3f6cb0b20a8e7306
Davide Grassano <34096612+Crivella@users.noreply.github.com>
2026-07-22 10:15:36 +0200

    Apply suggestion from @Flamefire
    Co-authored-by: Alexander Grund <Flamefire@users.noreply.github.com>

--- easybuild/framework/easyblock.py
+++ easybuild/framework/easyblock.py
@@ -1460,7 +1460,7 @@ class EasyBlock:
 
         # include load/unload statements for dependencies
         deps = []
-        self.log.debug("List of deps considered to load in generated module: %s", self.toolchain.dependencies)
+        self.log.debug("List of deps considered to load in generated module: %s", self.cfg.dependencies())
         for dep in self.cfg.dependencies():
             if dep['build_only']:
                 self.log.debug("Skipping build dependency %s", dep)
